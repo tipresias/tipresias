4daec9cafeb6034ad63fd39012f09f2b82c79a56
Craig Franklin <cfranklin@marketplacer.com>
2018-09-12 14:42:22 +1000

    Clean up charts

--- notebooks/src/model/metrics.py
+++ notebooks/src/model/metrics.py
@@ -18,8 +18,8 @@ def measure_regressor(estimator, data, cv=5):
     estimator.fit(X_train, y_train)
     y_pred = estimator.predict(X_test)
 
-    return (np.mean(cross_val_score(estimator, X_train, y_train, scoring=make_scorer(regression_accuracy), cv=cv)),
-            np.mean(cross_val_score(estimator, X_train, y_train, scoring='neg_mean_absolute_error', cv=cv)) * -1,
+    return (cross_val_score(estimator, X_train, y_train, scoring=make_scorer(regression_accuracy), cv=cv),
+            cross_val_score(estimator, X_train, y_train, scoring='neg_mean_absolute_error', cv=cv) * -1,
             regression_accuracy(y_test, y_pred),
             mean_absolute_error(y_test, y_pred))
 
@@ -31,12 +31,12 @@ def measure_classifier(estimator, data, cv=5):
     y_pred = estimator.predict(X_test)
 
     try:
-        cv_error_score = np.mean(cross_val_score(estimator, X_train, y_train, scoring='neg_log_loss', cv=cv))
+        cv_error_score = cross_val_score(estimator, X_train, y_train, scoring='neg_log_loss', cv=cv)
         test_error_score = log_loss(y_test, y_pred)
     except AttributeError:
-        cv_error_score, test_error_score = 'NA', 'NA'
+        cv_error_score, test_error_score = 0, 0
 
-    return (np.mean(cross_val_score(estimator, X_train, y_train, scoring='accuracy', cv=cv)),
+    return (cross_val_score(estimator, X_train, y_train, scoring='accuracy', cv=cv),
             cv_error_score,
             accuracy_score(y_test, y_pred),
             test_error_score)
@@ -49,37 +49,40 @@ def measure_estimators(estimators, data, model_type='regression', cv=5):
     scaler = StandardScaler()
 
     estimator_names = []
-    cv_accuracies = []
+    mean_cv_accuracies = []
     test_accuracies = []
-    cv_errors = []
+    mean_cv_errors = []
     test_errors = []
+    std_cv_accuracies = []
+    std_cv_errors = []
 
     for estimator in estimators:
         pipeline = make_pipeline(scaler, estimator)
 
         estimator_name = type(estimator).__name__
         if model_type == 'regression':
-            cv_accuracy, cv_error, test_accuracy, test_error = measure_regressor(pipeline, data, cv=cv)
+            cv_accuracies, cv_errors, test_accuracy, test_error = measure_regressor(pipeline, data, cv=cv)
         else:
-            cv_accuracy, cv_error, test_accuracy, test_error = measure_classifier(pipeline, data, cv=cv)
+            cv_accuracies, cv_errors, test_accuracy, test_error = measure_classifier(pipeline, data, cv=cv)
 
-        print(f'\n\n{estimator_name}')
-        print('Mean CV accuracy:', cv_accuracy)
-        print('Test accuracy:', test_accuracy)
-
-
-        print('\nMean CV negative error score:', cv_error)
-        print('Test error score:', test_error)
+        mean_cv_accuracy = np.mean(cv_accuracies)
+        mean_cv_error = np.mean(cv_errors)
+        std_cv_accuracy = np.std(cv_accuracies)
+        std_cv_error = np.std(cv_errors)
 
         estimator_names.append(estimator_name)
-        cv_accuracies.append(cv_accuracy)
+        mean_cv_accuracies.append(mean_cv_accuracy)
+        std_cv_accuracies.append(std_cv_accuracy)
         test_accuracies.append(test_accuracy)
-        cv_errors.append(cv_error)
+        mean_cv_errors.append(mean_cv_error)
+        std_cv_errors.append(std_cv_error)
         test_errors.append(test_error)
 
     score_types = ['cv'] * len(estimator_names) + ['test'] * len(estimator_names)
 
     return pd.DataFrame({'estimator': estimator_names * 2,
-                         'accuracy': cv_accuracies + test_accuracies,
-                         'error': cv_errors + test_errors,
+                         'accuracy': mean_cv_accuracies + test_accuracies,
+                         'error': mean_cv_errors + test_errors,
+                         'std_accuracy': std_cv_accuracies + [np.nan] * len(test_accuracies),
+                         'std_error': std_cv_errors + [np.nan] * len(test_errors),
                          'score_type': score_types})

f2805e4f3d1bbe3472c9e51aac38ad9808ea02a3
Craig Franklin <craigjfranklin@gmail.com>
2018-09-29 22:27:48 +1000

    Remove masonite code

--- app/http/controllers/HomeController.py
+++
@@ -1,7 +0,0 @@
-class HomeController:
-    """Controller root"""
-
-    def show(self, View):  # pylint: disable=R0201
-        """Show Welcome Template"""
-
-        return View('/storage/compiled/index')  # pylint: disable=E0602
--- server/views.py
+++ server/views.py
@@ -1,3 +1,20 @@
-from django.shortcuts import render
+import os
+import sys
+import pandas as pd
+from django.http import JsonResponse
 
-# Create your views here.
+PROJECT_PATH = os.path.abspath(
+    os.path.join(os.path.dirname(__file__), '../')
+)
+
+if PROJECT_PATH not in sys.path:
+    sys.path.append(PROJECT_PATH)
+
+
+def predictions(request):  # pylint: disable=W0613
+    """Render JSON data of model predictions"""
+
+    data_frame = pd.read_csv(f'{PROJECT_PATH}/data/model_predictions.csv')
+    data = {'data': data_frame.to_dict('records')}
+
+    return JsonResponse(data)

af9611ff8827f6c006f4760c1171ba7f86559c39
Craig Franklin <craigjfranklin@gmail.com>
2018-10-16 20:54:25 +1100

    Test player rating model

--- notebooks/src/model/metrics.py
+++ notebooks/src/model/metrics.py
@@ -13,30 +13,41 @@ def regression_accuracy(y, y_pred, **kwargs):
     return np.mean(correct_preds.astype(int))
 
 
-def measure_regressor(estimator, data, cv=5, n_jobs=-1):
+def measure_regressor(estimator, data, cv=5, n_jobs=-1, accuracy=True):
     # Data assumes we've used train_test_split outside of the function to guarantee
     # consistent data splits
     X_train, X_test, y_train, y_test = data
     estimator.fit(X_train, y_train)
     y_pred = estimator.predict(X_test)
 
-    return (cross_val_score(estimator,
-                            X_train,
-                            y_train,
-                            scoring=make_scorer(regression_accuracy),
-                            cv=cv,
-                            n_jobs=n_jobs),
+    if accuracy:
+        return (cross_val_score(estimator,
+                                X_train,
+                                y_train,
+                                scoring=make_scorer(regression_accuracy),
+                                cv=cv,
+                                n_jobs=n_jobs),
+                cross_val_score(estimator,
+                                X_train,
+                                y_train,
+                                scoring='neg_mean_absolute_error',
+                                cv=cv,
+                                n_jobs=n_jobs) * -1,
+                regression_accuracy(y_test, y_pred),
+                mean_absolute_error(y_test, y_pred))
+
+    return (0,
             cross_val_score(estimator,
                             X_train,
                             y_train,
                             scoring='neg_mean_absolute_error',
                             cv=cv,
                             n_jobs=n_jobs) * -1,
-            regression_accuracy(y_test, y_pred),
+            0,
             mean_absolute_error(y_test, y_pred))
 
 
-def measure_classifier(estimator, data, cv=5):
+def measure_classifier(estimator, data, cv=5, n_jobs=-1):
     # Data assumes we've used train_test_split outside of the function to guarantee
     # consistent data splits
     X_train, X_test, y_train, y_test = data
@@ -45,18 +56,18 @@ def measure_classifier(estimator, data, cv=5):
 
     try:
         cv_error_score = cross_val_score(
-            estimator, X_train, y_train, scoring='neg_log_loss', cv=cv)
+            estimator, X_train, y_train, scoring='neg_log_loss', cv=cv, n_jobs=n_jobs)
         test_error_score = log_loss(y_test, y_pred)
     except AttributeError:
         cv_error_score, test_error_score = 0, 0
 
-    return (cross_val_score(estimator, X_train, y_train, scoring='accuracy', cv=cv),
+    return (cross_val_score(estimator, X_train, y_train, scoring='accuracy', cv=cv, n_jobs=n_jobs),
             cv_error_score,
             accuracy_score(y_test, y_pred),
             test_error_score)
 
 
-def measure_estimators(estimators, data, model_type='regression', cv=5, n_jobs=-1):
+def measure_estimators(estimators, data, model_type='regression', cv=5, n_jobs=-1, accuracy=True):
     if model_type not in ('regression', 'classification'):
         raise Exception(
             f'model_type must be "regression" or "classification", but {model_type} was given.')
@@ -80,7 +91,8 @@ def measure_estimators(estimators, data, model_type='regression', cv=5, n_jobs=-
 
         if model_type == 'regression':
             cv_accuracies, cv_errors, test_accuracy, test_error = measure_regressor(
-                pipeline, data, cv=cv, n_jobs=n_jobs)
+                pipeline, data, cv=cv, n_jobs=n_jobs, accuracy=accuracy
+            )
         else:
             cv_accuracies, cv_errors, test_accuracy, test_error = measure_classifier(
                 pipeline, data, cv=cv, n_jobs=n_jobs)
@@ -97,6 +109,8 @@ def measure_estimators(estimators, data, model_type='regression', cv=5, n_jobs=-
         mean_cv_errors.append(mean_cv_error)
         std_cv_errors.append(std_cv_error)
         test_errors.append(test_error)
+
+        print(f'{estimator_name} done')
 
     score_types = ['cv'] * len(estimator_names) + \
         ['test'] * len(estimator_names)

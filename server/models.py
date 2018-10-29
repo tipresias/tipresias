a5879e4776ddc6f79ac59b674cc536efe2512ad3
Craig Franklin <cfranklin@marketplacer.com>
2018-10-29 15:34:48 +1100

    Add django models

--- server/models.py
+++ server/models.py
@@ -1,3 +1,35 @@
 from django.db import models
 
-# Create your models here.
+
+class Team(models.Model):
+    name = models.CharField(max_length=100)
+
+
+class Match(models.Model):
+    start_date_time = models.DateTimeField()
+    round_number = models.PositiveSmallIntegerField()
+
+    year = start_date_time.year
+
+
+class TeamMatch(models.Model):
+    team = models.ForeignKey(Team, on_delete=models.CASCADE)
+    match = models.ForeignKey(Match, on_delete=models.CASCADE)
+    at_home = models.BooleanField()
+    score = models.PositiveSmallIntegerField()
+
+
+class MLModel(models.Model):
+    trained_to_match = models.ForeignKey(
+        Match, on_delete=models.SET_NULL, null=True
+    )
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+    filepath = models.CharField(max_length=500)
+
+
+class Prediction(models.Model):
+    match = models.ForeignKey(Match, on_delete=models.CASCADE)
+    ml_model = models.ForeignKey(MLModel, on_delete=models.CASCADE)
+    predicted_winner = models.ForeignKey(Team, on_delete=models.CASCADE)
+    predicted_margin = models.SmallIntegerField()

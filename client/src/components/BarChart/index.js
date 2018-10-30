5c4bc882c8092af67579be6712dc9478cfb89781
melissa gattoni <meligatt@gmail.com>
2018-10-30 16:58:32 +1100

    add bar chart svg

--- client/src/components/BarChart/index.js
+++ client/src/components/BarChart/index.js
@@ -26,16 +26,7 @@ class BarChart extends Component {
   yAxis = d3.axisLeft().tickFormat(d => d);
 
   componentDidMount() {
-    // year/round/model
-
     const { gamesByYear } = this.props;
-
-    const [xMin, xMax] = d3.extent(gamesByYear, d => d.round_number);
-
-    const xScale = d3.scaleLinear()
-      .domain([xMin, xMax + 1])
-      .range([margin.left, width - margin.right]);
-
 
     // prepare models objects
     const modelsObject = gamesByYear.reduce((acc, currentItem, currentIndex, arr) => {
@@ -55,22 +46,13 @@ class BarChart extends Component {
 
       return acc;
     }, {});
-
     // console.log(modelsObject);
 
     const roundsArray = Object.keys(modelsObject);
 
-    // console.log(roundsArray);
-    const data = roundsArray.map((currentRound, index) => {
+    const cumulativeTipPointPerModel = roundsArray.map((currentRound, index) => {
       const modelKeyArray = Object.keys(modelsObject[currentRound]);
-      // console.log(modelKeyArray);
-
       const dataModels = modelKeyArray.map((model) => {
-        // console.log('currentRound >>', currentRound);
-        // console.log('prev round >>', currentRound - 1);
-        // console.log('modelsObject[currentRound][model] >> ', modelsObject[currentRound][model]);
-        // console.log('modelsObject[currentRound - 1][model] >> ', index > 0 ? modelsObject[currentRound - 1][model] : {});
-
         const prevRound = currentRound - 1;
         const currentModel = modelsObject[currentRound][model];
         let prevModel;
@@ -84,108 +66,119 @@ class BarChart extends Component {
         }
         const cumulativeTotalPoints = currentModel.total_points + prevModel.total_points;
         currentModel.total_points = cumulativeTotalPoints;
-        return { [model]: cumulativeTotalPoints };
+        return { model, cumulativeTotalPoints };
       });
-      // console.log(dataModels);
       return dataModels;
     });
 
-    console.log(data);
+    // console.log(cumulativeTipPointPerModel);
+
+    const [xMin, xMax] = d3.extent(gamesByYear, d => d.round_number);
+    const xScale = d3.scaleLinear()
+      .domain([xMin, xMax + 1])
+      .range([margin.left, width - margin.right]);
 
 
-    // const [min, max] = d3.extent(collection, d => d.cumulativeTipPoint);
-    // const yScale = d3.scaleLinear()
-    //   .domain([0, max])
-    //   .range([height - margin.bottom, margin.top]);
+    const [yMin, yMax] = d3.extent(cumulativeTipPointPerModel[27], item => item.cumulativeTotalPoints);
+    const yScale = d3.scaleLinear()
+      .domain([0, yMax])
+      .range([height - margin.bottom, margin.top]);
 
-    // const colorScale = d3.scaleOrdinal(d3.schemeCategory10);
+    const colorScale = d3.scaleOrdinal(d3.schemeCategory10);
 
-    // for (let i = ROUND_MIN; i <= ROUND_MAX; i += 1) {
-    //   // console.log(i);
-    // }
+    // console.log(cumulativeTipPointPerModel);
 
-    // const bars = collection.map((d) => {
-    //   let x;
+    const bars = cumulativeTipPointPerModel.map((roundItem, roundItemIndex) => {
+      const barsPerRound = roundItem.map((modelItem) => {
+        let x;
 
-    //   if (d.model === 'oddsmakers') {
-    //     x = xScale(d.round_number);
-    //   }
-    //   if (d.model === 'tipresias_betting') {
-    //     x = xScale(d.round_number) + 10;
-    //   }
-    //   if (d.model === 'tipresias_match') {
-    //     x = xScale(d.round_number) + 20;
-    //   }
+        if (modelItem.model === 'oddsmakers') {
+          x = xScale(roundItemIndex);
+        }
+        if (modelItem.model === 'tipresias_betting') {
+          x = xScale(roundItemIndex) + 10;
+        }
+        if (modelItem.model === 'tipresias_match') {
+          x = xScale(roundItemIndex) + 20;
+        }
 
-    //   const y = yScale(d.cumulativeTipPoint);
-    //   const h = yScale(0) - yScale(d.cumulativeTipPoint);
+        const y = yScale(modelItem.cumulativeTotalPoints);
+        const h = yScale(0) - yScale(modelItem.cumulativeTotalPoints);
 
-    //   return ({
-    //     key: `${d.model}-${d.round_number}`,
-    //     round: d.round_number,
-    //     x,
-    //     y,
-    //     height: h,
-    //     width: 6,
-    //     fill: colorScale(d.model),
-    //   });
-    // });
+        return ({
+          key: `${roundItemIndex + 1}-${modelItem.model}`,
+          round: roundItemIndex + 1,
+          x,
+          y,
+          height: h,
+          width: 6,
+          fill: colorScale(modelItem.model),
+        });
+      });
+      return barsPerRound;
+    });
 
-
-    // this.setState({
-    //   bars,
-    //   xScale,
-    //   yScale,
-    //   calculating: false,
-    // });
+    this.setState({
+      bars,
+      xScale,
+      yScale,
+      calculating: false,
+    });
   }
 
   componentDidUpdate() {
-    // const { xScale, yScale } = this.state;
-    // this.xAxis.scale(xScale);
-    // d3.select(this.xAxisRef.current).call(this.xAxis);
-    // this.yAxis.scale(yScale);
-    // d3.select(this.yAxisRef.current).call(this.yAxis);
+    const { xScale, yScale } = this.state;
+    this.xAxis.scale(xScale);
+    d3.select(this.xAxisRef.current).call(this.xAxis);
+    this.yAxis.scale(yScale);
+    d3.select(this.yAxisRef.current).call(this.yAxis);
   }
 
   render() {
-    return <div>WIP</div>;
-    // const {
-    //   bars,
-    //   calculating,
-    // } = this.state;
-    // const { year } = this.props;
+    const {
+      bars,
+      calculating,
+    } = this.state;
+    console.log(bars);
 
-    // const xAxisTranslate = `translate(0, ${height - margin.bottom})`;
-    // const yAxisTranslate = `translate(${margin.left},0)`;
-    // return (
-    //   <div>
-    //     <p>
-    //       BarChart for
-    //       {year}
-    //     </p>
-    //     {
-    //       !calculating && (
-    //         <svg width="800" height="400" style={{ border: '1px solid black' }}>
-    //           {
-    //             bars.map(item => (<rect
-    //               key={item.key}
-    //               x={item.x}
-    //               y={item.y}
-    //               width={item.width}
-    //               height={item.height}
-    //               fill={item.fill}
-    //               stroke={item.stroke}
-    //               strokeWidth={item.strokeWidth}
-    //             />
-    //             ))
-    //           }
-    //           <g ref={this.xAxisRef} transform={xAxisTranslate} />
-    //           <g ref={this.yAxisRef} transform={yAxisTranslate} />
-    //         </svg>)
-    //     }
-    //   </div>
-    // );
+    const { year } = this.props;
+    const title = `BarChart for ${year}`;
+
+    const xAxisTranslate = `translate(0, ${height - margin.bottom})`;
+    const yAxisTranslate = `translate(${margin.left},0)`;
+    return (
+      <div>
+        <p>
+          {title}
+        </p>
+        {
+          !calculating && (
+            <svg width="800" height="400" style={{ border: '1px solid black' }}>
+              {
+                bars.map((item, index) => (
+                  <g key={index}>
+                    {
+                      item.map(i => (<rect
+                        key={i.key}
+                        x={i.x}
+                        y={i.y}
+                        width={i.width}
+                        height={i.height}
+                        fill={i.fill}
+                        stroke={i.stroke}
+                        strokeWidth={i.strokeWidth}
+                      />
+                      ))
+                    }
+                  </g>
+                ))
+              }
+              <g ref={this.xAxisRef} transform={xAxisTranslate} />
+              <g ref={this.yAxisRef} transform={yAxisTranslate} />
+            </svg>)
+        }
+      </div>
+    );
   }
 }
 export default BarChart;

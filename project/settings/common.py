c67677b6d93d3c62e340734546d920ef2d36fe00
Craig Franklin <craigjfranklin@gmail.com>
2018-11-03 18:27:43 +1100

    Update database settings

--- project/settings/common.py
+++ project/settings/common.py
@@ -59,16 +59,7 @@ WSGI_APPLICATION = 'project.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.1/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'tipresias',
-        'HOST': os.getenv('DATABASE_HOST'),
-        'USER': 'postgres',
-        'PASSWORD': '',
-        'PORT': 5432
-    }
-}
+DATABASES = {}
 
 
 # Password validation
--- project/settings/development.py
+++ project/settings/development.py
@@ -1,3 +1,4 @@
+import os
 # pylint: disable=W0401,W0614
 from project.settings.common import *
 
@@ -8,3 +9,14 @@ DEBUG = True
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = '^6&#7de5dx#eqg6dm^l3#@wj6vjjn%2f=u(!&ia()h-l1ppan!'
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'tipresias',
+        'HOST': os.getenv('DATABASE_HOST'),
+        'USER': 'postgres',
+        'PASSWORD': '',
+        'PORT': 5432
+    }
+}

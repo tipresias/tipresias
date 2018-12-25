daeb99f85138825cbce03c004727311caa8e9414
Craig Franklin <craigjfranklin@gmail.com>
2018-12-25 17:47:43 +1100

    Update data models

--- backend/server/models.py
+++ backend/server/models.py
@@ -1,3 +1,5 @@
+from functools import reduce
+from datetime import datetime, timezone, timedelta
 from django.db import models
 from django.core.exceptions import ValidationError
 from django.utils.translation import gettext_lazy as _
@@ -24,6 +26,8 @@ TEAM_NAMES = [
     "Fitzroy",
     "University",
 ]
+# Rough estimate, but exactitude isn't necessary here
+GAME_LENGTH_HRS = 3
 
 
 def validate_name(name):
@@ -42,26 +46,54 @@ class Match(models.Model):
     round_number = models.PositiveSmallIntegerField()
 
     @property
+    def is_draw(self):
+        return self.has_been_played and reduce(
+            lambda x, y: x.score == y.score, self.teammatch_set.all()
+        )
+
+    @property
     def winner(self):
+        if not self.has_been_played or self.is_draw:
+            return None
+
         return max(self.teammatch_set.all(), key=lambda x: x.score).team
 
     @property
     def year(self):
         return self.start_date_time.year
 
+    @property
+    def has_been_played(self):
+        # We need to check the scores in case the data hasn't been updated since the
+        # match was played, because as far as the data is concerned it hasn't, even though
+        # the date has passed.
+        return self.__has_score and (
+            self.start_date_time
+            < datetime.now(timezone.utc) - timedelta(hours=GAME_LENGTH_HRS)
+        )
+
+    @property
+    def __has_score(self):
+        return any(
+            [
+                score > 0
+                for score in self.teammatch_set.all().values_list("score", flat=True)
+            ]
+        )
+
 
 class TeamMatch(models.Model):
     team = models.ForeignKey(Team, on_delete=models.CASCADE)
     match = models.ForeignKey(Match, on_delete=models.CASCADE)
     at_home = models.BooleanField()
-    score = models.PositiveSmallIntegerField()
+    score = models.PositiveSmallIntegerField(default=0)
 
 
 class MLModel(models.Model):
     trained_to_match = models.ForeignKey(
         Match, on_delete=models.SET_NULL, null=True, blank=True
     )
-    name = models.CharField(max_length=100)
+    name = models.CharField(max_length=100, unique=True)
     description = models.TextField(null=True, blank=True)
     filepath = models.CharField(max_length=500, null=True, blank=True)
 
@@ -74,4 +106,6 @@ class Prediction(models.Model):
 
     @property
     def is_correct(self):
-        return self.predicted_winner == self.match.winner
+        return self.match.has_been_played and (
+            self.match.is_draw or self.predicted_winner == self.match.winner
+        )

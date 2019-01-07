e9f3516ef12dfceb35d94e9fc5897a89b5938414
melissa gattoni <meligatt@gmail.com>
2019-01-08 07:46:44 +1100

    Add test wip

--- frontend/src/services/fetchPredictions/__tests__/index.js
+++ frontend/src/services/fetchPredictions/__tests__/index.js
@@ -1,14 +1,25 @@
+import nock from 'nock';
+import axios from 'axios';
+import httpAdapter from 'axios/lib/adapters/http';
 import fetchPredictions from '../index';
 
-describe('fetchPredictions', () => {
-  it('renders', () => {
-    // arrange
-    // act
-    // assert
-    fetchPredictions().then((response) => {
-      console.log(response);
+const host = 'http://localhost:3000';
+axios.defaults.host = host;
+axios.defaults.adapter = httpAdapter;
 
-      expect(true).toBe(true);
+describe('fetchPredictions', () => {
+  it('sends a HTTP request with method set as GET', () => {
+    // arrange
+    const endpoint = '/fake/endpoint';
+
+    const scope = nock(host)
+      .get(endpoint)
+      .reply(200, '{"lorem":"ipsum"}');
+
+    // act
+    return fetchPredictions().then(() => {
+      // assert
+      expect(scope.isDone()).toBe(true);
     });
   });
 });

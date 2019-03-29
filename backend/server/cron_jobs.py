f5a7399806dd20879d5049410e77df1c7a5cdd05
Craig Franklin <cfranklin@marketplacer.com>
2019-03-29 11:20:09 +1100

    Define conditions for running tip & send_email commands

--- backend/server/cron_jobs.py
+++ backend/server/cron_jobs.py
@@ -1,8 +1,18 @@
 """Cron jobs to be run in production"""
 
+from datetime import datetime, timezone
 from django_cron import CronJobBase, Schedule
+import pandas as pd
+
+# TODO: These commands need to be refactored, then the refactored classes/functions
+# used in this cron job, but I want to get something working, so I'm gonna be lazy
+from server.management.commands import tip, send_email
+from server.data_readers import FootywireDataReader
 
 MINUTES_PER_HOUR = 60
+# Note that Python starts weeks on Monday (index = 0) and ends them on Sunday
+# (index = 6)
+SATURDAY = 5
 
 
 class SendTips(CronJobBase):
@@ -12,8 +22,54 @@ class SendTips(CronJobBase):
     # plenty of time to submit my tips.
     RUN_AT_TIMES = ["12:00"]
 
-    schedule = Schedule(run_every_mins=RUN_EVERY_MINS)
+    schedule = Schedule(run_at_times=RUN_AT_TIMES)
     code = "tipresias.send_tips"
 
+    def __init__(self, data_reader=FootywireDataReader()):
+        super().__init__()
+
+        # TODO: Need to set server timezone to Melbourne time for this to work as
+        # intended
+        self.right_now = datetime.now(tz=timezone.utc)
+        self.data_reader = data_reader
+
     def do(self):
-        pass
+        fixture_data_frame = self.__fetch_fixture_data(self.right_now.year)
+        fixture_dates = pd.to_datetime(fixture_data_frame["date"]).dt.date
+        is_before_saturday = self.right_now.date().weekday() < SATURDAY
+
+        # This is to make sure that we have roster data for tips, because for rounds
+        # Thursday games, we have to tip on Thursday before rosters are announced for
+        # the rest of the matches (they usually get announced on Thursday around
+        # 6:30 pm). So, we'll want to update tips for the rest of the round on Friday.
+        if self.__is_match_today(fixture_dates) and is_before_saturday:
+            pass
+
+    def __fetch_fixture_data(self, year: int) -> pd.DataFrame:
+        fixture_data_frame = self.data_reader.get_fixture(
+            year_range=(year, year + 1), fetch_data=True
+        ).assign(date=lambda df: df["date"].dt.tz_localize(timezone.utc))
+
+        latest_match = fixture_data_frame["date"].max()
+
+        if self.right_now > latest_match:
+            print(
+                f"No unplayed matches found in {year}. We will try to fetch "
+                f"fixture for {year + 1}.\n"
+            )
+
+            fixture_data_frame = self.data_reader.get_fixture(
+                year_range=(year + 1, year + 2), fetch_data=True
+            )
+            latest_match = fixture_data_frame["date"].max()
+
+            if self.right_now > latest_match:
+                raise ValueError(
+                    f"No unplayed matches found in {year + 1}, and we're not going "
+                    "to keep trying. Please try a season that hasn't been completed.\n"
+                )
+
+        return fixture_data_frame
+
+    def __is_match_today(self, fixture_dates: pd.Series) -> bool:
+        return fixture_dates[fixture_dates == self.right_now.date].any()
--- backend/server/tests/unit/test_cron_jobs.py
+++ backend/server/tests/unit/test_cron_jobs.py
@@ -1,12 +1,57 @@
 from unittest.mock import Mock, patch
+from datetime import date, datetime
+
 from django.test import TestCase
+from freezegun import freeze_time
+import pandas as pd
 
 from server.cron_jobs import SendTips
+from server.data_readers import FootywireDataReader
+
+THURSDAY = "2019-3-28"
+FRIDAY = "2019-3-29"
+SATURDAY = "2019-3-30"
+SEVEN_O_CLOCK = 7
 
 
 class TestSendTips(TestCase):
     def setUp(self):
-        self.job = SendTips()
+        friday_args = [int(date_str) for date_str in FRIDAY.split("-")]
+        friday = date(*friday_args)
+        year = friday.year
+        fixture_data_frame = pd.DataFrame(
+            [
+                {
+                    "date": datetime(year, friday.month, friday.day, SEVEN_O_CLOCK),
+                    "home_team": "Richmond",
+                    "away_team": "Carlton",
+                    "home_score": 0,
+                    "away_score": 0,
+                    "venue": "MCG",
+                    "crowd": 0,
+                    "round_label": "Round 1",
+                    "round": 1,
+                    "season": year,
+                },
+                {
+                    "date": datetime(year, friday.month, friday.day + 1, SEVEN_O_CLOCK),
+                    "home_team": "Melbourne",
+                    "away_team": "Sydney",
+                    "home_score": 0,
+                    "away_score": 0,
+                    "venue": "MCG",
+                    "crowd": 0,
+                    "round_label": "Round 1",
+                    "round": 1,
+                    "season": year,
+                },
+            ]
+        )
+
+        data_reader = FootywireDataReader()
+        data_reader.get_fixture = Mock(return_value=fixture_data_frame)
+
+        self.job = SendTips(data_reader=data_reader)
 
     def test_do(self):
         with patch("server.management.commands.tip.Command") as MockTipCommand:
@@ -15,7 +60,24 @@ class TestSendTips(TestCase):
                 "server.management.commands.send_email.Command"
             ) as MockSendCommand:
                 MockSendCommand.return_value.handle = Mock()
-                self.job.do()
 
-                MockTipCommand.handle.assert_called()
-                MockSendCommand.handle.assert_called()
+                with freeze_time(FRIDAY):
+                    with self.subTest("on Friday with a match"):
+                        self.job.do()
+
+                        MockTipCommand.handle.assert_called()
+                        MockSendCommand.handle.assert_called()
+
+                with freeze_time(THURSDAY):
+                    with self.subTest("on Thursday without a match"):
+                        self.job.do()
+
+                        MockTipCommand.handle.assert_not_called()
+                        MockSendCommand.handle.assert_not_called()
+
+                with freeze_time(SATURDAY):
+                    with self.subTest("on Saturday with a match"):
+                        self.job.do()
+
+                        MockTipCommand.handle.assert_not_called()
+                        MockSendCommand.handle.assert_not_called()

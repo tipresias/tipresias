acce6f74709adc46f6f43641bd62f9d9867b73fe
Craig Franklin <craigjfranklin@gmail.com>
2019-04-05 09:42:58 +1100

    Use clone repo link directly to avoid GitHub API calls

--- backend/requirements.r
+++ backend/requirements.r
@@ -1,2 +1,2 @@
 install.packages('remotes', repos='https://mirror.aarnet.edu.au/pub/CRAN/')
-remotes::install_github('jimmyday12/fitzRoy')
+remotes::install_github('git@github.com:jimmyday12/fitzRoy.git')

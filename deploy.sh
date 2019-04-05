f1742c565e8f18cde2a1079616f3866e2fbc1424
Craig Franklin <craigjfranklin@gmail.com>
2019-04-06 09:35:13 +1100

    Fix permissions for ssh?

--- deploy.sh
+++ deploy.sh
@@ -2,5 +2,7 @@ echo "$DOCKER_PASSWORD" | docker login -u cfranklin11 --password-stdin
 docker pull cfranklin11/tipresias_app:latest
 docker build --cache-from cfranklin11/tipresias_app:latest -t cfranklin11/tipresias_app:latest .
 docker push cfranklin11/tipresias_app:latest
+sudo chmod 600 ~/.ssh/deploy_rsa
+sudo chmod 755 ~/.ssh
 scp -i ~/.ssh/deploy_rsa docker-compose.prod.yml ${DEPLOY_USER}@${IP_ADDRESS}:~/tipresias/docker-compose.yml
 ssh -i ~/.ssh/deploy_rsa ${DEPLOY_USER}@${IP_ADDRESS} "docker pull cfranklin11/tipresias_app:latest && docker-compose -f ./tipresias/docker-compose.yml up -d --build"

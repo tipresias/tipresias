98a4f5b27bfd5220175648044ecffdef1d077ec9
Craig Franklin <craigjfranklin@gmail.com>
2019-04-05 19:55:57 +1100

    Try to fix ssh deploy issues

--- deploy.sh
+++ deploy.sh
@@ -2,5 +2,5 @@ echo "$DOCKER_PASSWORD" | docker login -u cfranklin11 --password-stdin
 docker pull cfranklin11/tipresias_app:latest
 docker build --cache-from cfranklin11/tipresias_app:latest -t cfranklin11/tipresias_app:latest .
 docker push cfranklin11/tipresias_app:latest
-scp -i deploy_rsa docker-compose.prod.yml ${DEPLOY_USER}@${IP_ADDRESS}:~/tipresias/docker-compose.yml
-ssh -i deploy_rsa ${DEPLOY_USER}@${IP_ADDRESS} "docker pull cfranklin11/tipresias_app:latest && docker-compose -f ./tipresias/docker-compose.yml up -d --build"
+scp -i ~/.ssh/deploy_rsa docker-compose.prod.yml ${DEPLOY_USER}@${IP_ADDRESS}:~/tipresias/docker-compose.yml
+ssh -i ~/.ssh/deploy_rsa ${DEPLOY_USER}@${IP_ADDRESS} "docker pull cfranklin11/tipresias_app:latest && docker-compose -f ./tipresias/docker-compose.yml up -d --build"

d2fea3ee5631d6e13c299536bd884576cfb5fef7
Craig Franklin <cfranklin@marketplacer.com>
2019-04-23 10:28:54 +1000

    Fix dtypes for imported betting data

--- backend/machine_learning/tests/integration/data_import/test_footywire_data_importer.py
+++ backend/machine_learning/tests/integration/data_import/test_footywire_data_importer.py
@@ -23,6 +23,7 @@ class TestFootywireDataImporter(TestCase):
             self.assertEqual(len(seasons), 1)
             self.assertEqual(seasons.iloc[0], 2004)
 
+            self.assertEqual(data_frame["date"].dtype, "datetime64[ns]")
             date_years = data_frame["date"].dt.year.drop_duplicates()
             self.assertEqual(len(date_years), 1)
             self.assertEqual(date_years.iloc[0], 2004)
@@ -32,6 +33,7 @@ class TestFootywireDataImporter(TestCase):
 
             self.assertIsInstance(data_frame, pd.DataFrame)
             self.assertFalse(data_frame.empty)
+            self.assertEqual(data_frame["date"].dtype, "datetime64[ns]")
 
         with self.subTest("when fetch_data is False and year_range is specified"):
             data_frame = self.data_reader.get_fixture(
@@ -55,9 +57,8 @@ class TestFootywireDataImporter(TestCase):
             self.assertEqual(len(seasons), 1)
             self.assertEqual(seasons.iloc[0], 2014)
 
-            # 'date' column is python datetime, not pandas datetime, so we have to
-            # convert it before calling .dt
-            date_years = pd.to_datetime(data_frame["date"]).dt.year.drop_duplicates()
+            self.assertEqual(data_frame["date"].dtype, "datetime64[ns]")
+            date_years = data_frame["date"].dt.year.drop_duplicates()
             self.assertEqual(len(date_years), 1)
             self.assertEqual(date_years.iloc[0], 2014)
 
@@ -66,6 +67,7 @@ class TestFootywireDataImporter(TestCase):
 
             self.assertIsInstance(data_frame, pd.DataFrame)
             self.assertFalse(data_frame.empty)
+            self.assertEqual(data_frame["date"].dtype, "datetime64[ns]")
 
         with self.subTest("when fetch_data is False and year_range is specified"):
             data_frame = self.data_reader.get_betting_odds(

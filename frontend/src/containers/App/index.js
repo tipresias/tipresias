5a3796a0aad0a3485ed3d3ae1e7bc705154ab65b
melissa gattoni <meligatt@gmail.com>
2019-04-27 21:14:10 +1000

    Add test to render data from predictions endpoint in App container

--- frontend/src/containers/App/index.js
+++ frontend/src/containers/App/index.js
@@ -7,6 +7,7 @@ import logo from './tipresias-logo.svg';
 import './App.css';
 import BarChartContainer from '../BarChartContainer';
 import Select from '../../components/Select';
+import Predictions from '../../components/Predictions';
 
 type State = {
   isLoading: boolean,
@@ -25,64 +26,67 @@ class App extends Component<Props, State> {
     gamesByYear: [],
   };
 
-  componentDidMount() {
-    fetchPredictions('/predictions').then((data) => {
-      this.setState({ allGames: data }, () => {
-        const { allGames, year } = this.state;
-        this.setGamesByYear(allGames, year);
-      });
-    }).catch((err) => {
-      console.log(err);
-    });
-  }
+  // componentDidMount() {
+  //   fetchPredictions('/predictions').then((data) => {
+  //     this.setState({ allGames: data }, () => {
+  //       const { allGames, year } = this.state;
+  //       this.setGamesByYear(allGames, year);
+  //     });
+  //   }).catch((err) => {
+  //     console.log(err);
+  //   });
+  // }
 
-  componentDidUpdate(prevProps: Props, prevState: State) {
-    const { allGames, year } = this.state;
-    if (year !== prevState.year) {
-      this.setGamesByYear(allGames, year);
-    }
-  }
+  // componentDidUpdate(prevProps: Props, prevState: State) {
+  //   const { allGames, year } = this.state;
+  //   if (year !== prevState.year) {
+  //     this.setGamesByYear(allGames, year);
+  //   }
+  // }
 
-  onChangeYear = (event: SyntheticEvent<HTMLSelectElement>): void => {
-    this.setState({ year: parseInt(event.currentTarget.value, 10) });
-  }
+  // onChangeYear = (event: SyntheticEvent<HTMLSelectElement>): void => {
+  //   this.setState({ year: parseInt(event.currentTarget.value, 10) });
+  // }
 
-  setGamesByYear(allGames: Array<Game>, year: number) {
-    const gamesByYear = filterDataByYear(allGames, year);
-    this.setState({
-      gamesByYear,
-      isLoading: false,
-    });
-  }
+  // setGamesByYear(allGames: Array<Game>, year: number) {
+  //   const gamesByYear = filterDataByYear(allGames, year);
+  //   this.setState({
+  //     gamesByYear,
+  //     isLoading: false,
+  //   });
+  // }
 
+  // render() {
+  //   const {
+  //     isLoading,
+  //     gamesByYear,
+  //     year,
+  //   } = this.state;
+
+  //   let contentComponent;
+  //   if (isLoading) {
+  //     contentComponent = <div>Loading content!...</div>;
+  //   } else {
+  //     contentComponent = <BarChartContainer year={year} gamesByYear={gamesByYear} />;
+  //   }
+  //   return (
+  //     <div className="App">
+  //       <header className="App-header">
+  //         <img src={logo} className="App-logo" alt="logo" />
+  //         <Select
+  //           value={year}
+  //           onChange={this.onChangeYear}
+  //           options={[2011, 2012, 2013, 2014]}
+  //         />
+  //       </header>
+  //       <div className="App-content">
+  //         {contentComponent}
+  //       </div>
+  //     </div>
+  //   );
+  // }
   render() {
-    const {
-      isLoading,
-      gamesByYear,
-      year,
-    } = this.state;
-
-    let contentComponent;
-    if (isLoading) {
-      contentComponent = <div>Loading content!...</div>;
-    } else {
-      contentComponent = <BarChartContainer year={year} gamesByYear={gamesByYear} />;
-    }
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <Select
-            value={year}
-            onChange={this.onChangeYear}
-            options={[2011, 2012, 2013, 2014]}
-          />
-        </header>
-        <div className="App-content">
-          {contentComponent}
-        </div>
-      </div>
-    );
+    return (<Predictions />)
   }
 }
 

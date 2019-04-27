7db59a0e3fddaf8acd2057858a10f8455e0bd30f
Craig Franklin <craigjfranklin@gmail.com>
2019-04-27 14:00:33 +1000

    Fix docker command assignment?

--- deploy.sh
+++ deploy.sh
@@ -7,7 +7,6 @@ sudo chmod 600 ~/.ssh/deploy_rsa
 sudo chmod 755 ~/.ssh
 scp -i ~/.ssh/deploy_rsa docker-compose.prod.yml ${DEPLOY_USER}@${IP_ADDRESS}:~/tipresias/docker-compose.yml
 
-UPDATE_IMAGES_CMD=docker pull cfranklin11/tipresias_app:latest \
+ssh -i ~/.ssh/deploy_rsa ${DEPLOY_USER}@${IP_ADDRESS} "docker pull cfranklin11/tipresias_app:latest \
   && docker pull cfranklin11/tipresias_afl_data:latest \
-  && docker-compose -f ./tipresias/docker-compose.yml up -d --build
-ssh -i ~/.ssh/deploy_rsa ${DEPLOY_USER}@${IP_ADDRESS} "$UPDATE_IMAGES_COMMAND"
+  && docker-compose -f ./tipresias/docker-compose.yml up -d --build"

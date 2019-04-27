1ceaf031f0f130b575a44f33614f26d8f6b8fbfa
Craig Franklin <craigjfranklin@gmail.com>
2019-04-27 11:25:25 +1000

    Move functions back out of route function

--- afl_data/R/plumber.R
+++ afl_data/R/plumber.R
@@ -28,35 +28,35 @@ function(start_date = "1897-01-01", end_date = Sys.Date()) {
   data %>%
     rename_all(funs(str_to_lower %>% str_replace_all(., "\\.", "_"))) %>%
     jsonlite::toJSON()
+}
 
-  this_year <- Sys.Date() %>% substring(0, 4) %>% as.integer()
+handle_players_route_error <- function(start_date, end_date) {
+  end_date_year <- end_date %>% substring(0, 4) %>% as.integer()
 
-  handle_players_route_error <- function(start_date, end_date) {
-    end_date_year <- end_date %>% substring(0, 4) %>% as.integer()
-
-    function(err) {
-      if (end_date_year > this_year) {
-        retry_with_last_year_end_date(start_date, end_date)
-      } else {
-        stop(err)
-      }
+  function(err) {
+    if (end_date_year > this_year) {
+      retry_with_last_year_end_date(start_date, end_date)
+    } else {
+      stop(err)
     }
   }
-
-  retry_with_last_year_end_date <- function(start_date, end_date) {
-      end_date_last_year <- paste0(this_year - 1, "-12-31")
-
-      warning(
-        paste0(
-          "end_date of ", end_date, " is in a year for which AFLTables has no ",
-          "data. Retrying with an end_date of the end of last year: ",
-          end_date_last_year
-        )
-      )
-
-      fitzRoy::get_afltables_stats(
-        start_date = start_date,
-        end_date = end_date_last_year
-      )
-  }
 }
+
+retry_with_last_year_end_date <- function(start_date, end_date) {
+    end_date_last_year <- paste0(this_year - 1, "-12-31")
+
+    warning(
+      paste0(
+        "end_date of ", end_date, " is in a year for which AFLTables has no ",
+        "data. Retrying with an end_date of the end of last year: ",
+        end_date_last_year
+      )
+    )
+
+    fitzRoy::get_afltables_stats(
+      start_date = start_date,
+      end_date = end_date_last_year
+    )
+}
+
+this_year <- Sys.Date() %>% substring(0, 4) %>% as.integer()

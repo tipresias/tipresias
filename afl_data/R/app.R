c70bd6f2507e5a5ae62626f4a9c80fdc07824a60
Craig Franklin <craigjfranklin@gmail.com>
2019-04-27 10:56:53 +1000

    Make afl_data conform more-closely to R package conventions

---
+++ afl_data/R/app.R
@@ -0,0 +1,4 @@
+library("tidyverse")
+
+pr <- plumber::plumb("R/plumber.R")
+pr$run(host = "0.0.0.0", port = 8001)
--- afl_data/app.R
+++
@@ -1,4 +0,0 @@
-library("tidyverse")
-
-pr <- plumber::plumb("plumber.R")
-pr$run(host="0.0.0.0", port=8001)

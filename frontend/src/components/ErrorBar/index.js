7b0af2297f0cbbda730452831493d67c412714f2
melissa gattoni <meligatt@gmail.com>
2019-05-03 16:25:08 +1000

    Update app container

---
+++ frontend/src/components/ErrorBar/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const ErrorBar = ({
+  text
+}) => (
+    <div style={{ border: '1px solid red', backgroundColor: '#f5f5fb' }}>{text}</div>
+  );
+
+export default ErrorBar;
--- frontend/src/containers/App/index.js
+++ frontend/src/containers/App/index.js
@@ -1,87 +1,71 @@
 // @flow
-import React, { Fragment, Component } from 'react';
+import React, { Component } from 'react';
 import { Query } from 'react-apollo';
-import { getPredictionsQuery } from '../../graphql';
+import { GET_PREDICTIONS_QUERY } from '../../graphql';
 import type { Game } from '../../types';
-// import fetchPredictions from '../../services/fetchPredictions';
-// import filterDataByYear from '../../utils/filterGameByYear';
-import logo from './tipresias-logo.svg';
-import './App.css';
-// import BarChartContainer from '../BarChartContainer';
+import BarChartContainer from '../BarChartContainer';
 import Select from '../../components/Select';
+import Image from '../../components/Image';
+import ErrorBar from '../../components/ErrorBar';
+import LoadingBar from '../../components/LoadingBar';
+import EmptyChart from '../../components/EmptyChart';
 import BarChartSecondary from '../../components/BarChartSecondary';
 
 type State = {
   year: number
 }
 
-type Props = {
-}
+type Props = {}
 
 class App extends Component<Props, State> {
   state = {
     year: 2014
   };
 
-  // componentDidMount() {
-  // fetchPredictions('/predictions').then((data) => {
-  //   this.setState({ allGames: data }, () => {
-  //     const { allGames, year } = this.state;
-  //     // this.setGamesByYear(allGames, year);
-  //   });
-  // }).catch((err) => {
-  //   console.log(err);
-  // });
-  // }
-
-  // componentDidUpdate(prevProps: Props, prevState: State) {
-  // const { allGames, year } = this.state;
-  // if (year !== prevState.year) {
-  //   this.setGamesByYear(allGames, year);
-  // }
-  // }
+  OPTIONS = [2011, 2014, 2015, 2016, 2017];
 
   onChangeYear = (event: SyntheticEvent<HTMLSelectElement>): void => {
     this.setState({ year: parseInt(event.currentTarget.value, 10) });
   }
-
-  // setGamesByYear(allGames: Array<Game>, year: number) {
-  //   const gamesByYear = filterDataByYear(allGames, year);
-  //   this.setState({
-  //     gamesByYear,
-  //     isLoading: false,
-  //   });
-  // }
+  onSomethingElse = (event: SyntheticEvent<HTMLSelectElement>): void => {
+    this.setState({ year: parseInt(event.currentTarget.value, 10) });
+  }
 
   render() {
     const {
       year
     } = this.state;
 
-
-    // let contentComponent;
-    // if (isLoading) {
-    //   contentComponent = <div>Loading content!...</div>;
-    // } else {
-    //   contentComponent = <BarChartContainer year={year} gamesByYear={games} />;
-    // }
     const queryChildren = ({ loading, error, data }) => {
-      if (loading) return <div>loading...</div>
-      if (error) return <div>error...</div>
-      return <BarChartSecondary games={data} />
+      const nonNullData = (data || {})
+      const dataWithAllPredictions = { predictions: [], ...nonNullData }
+      const { predictions } = dataWithAllPredictions;
+
+      // if loading prop is true, render loading component
+      if (loading) return <LoadingBar text="Loading predictions..." />
+
+      // if error prop is true, render error component
+      if (error) return <ErrorBar text={error.message} />
+
+      // if predictions is empty
+      if (predictions.length === 0) return <EmptyChart text="No data found" />
+
+      // if predictions prop is passed, renders barChartContainer component
+      return <BarChartContainer games={predictions} />
     }
 
     return (
-      <div className="App">
+      <div className="App" style={{ backgroundColor: '#f3f3f3' }}>
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
+          <Image alt="Tipresias" width={120} />
           <Select
+            name="year"
             value={year}
             onChange={this.onChangeYear}
-            options={[2011, 2012, 2013, 2014, 2015, 2016, 2017]}
+            options={this.OPTIONS}
           />
         </header>
-        <Query query={getPredictionsQuery} variables={{ year }}>
+        <Query query={GET_PREDICTIONS_QUERY} variables={{ year }} onCompleted={() => console.log("data fetched!")}>
           {queryChildren}
         </Query>
       </div>

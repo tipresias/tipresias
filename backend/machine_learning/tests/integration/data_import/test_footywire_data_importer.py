3d4adb64460723baf35c0a547ef87e95ee11fe42
Craig Franklin <craigjfranklin@gmail.com>
2019-05-23 20:05:19 +1000

    Remove unused fixture data code

--- backend/machine_learning/tests/integration/data_import/test_footywire_data_importer.py
+++ backend/machine_learning/tests/integration/data_import/test_footywire_data_importer.py
@@ -16,40 +16,6 @@ class TestFootywireDataImporter(TestCase):
     def setUp(self):
         self.data_reader = FootywireDataImporter(csv_dir=DATA_DIR)
 
-    def test_get_fixture(self):
-        with self.subTest("when fetch_data is True"):
-            data_frame = self.data_reader.get_fixture(
-                year_range=(2004, 2005), fetch_data=True
-            )
-
-            self.assertIsInstance(data_frame, pd.DataFrame)
-
-            seasons = data_frame["season"].drop_duplicates()
-            self.assertEqual(len(seasons), 1)
-            self.assertEqual(seasons.iloc[0], 2004)
-
-            self.assertEqual(data_frame["date"].dtype, "datetime64[ns]")
-            date_years = data_frame["date"].dt.year.drop_duplicates()
-            self.assertEqual(len(date_years), 1)
-            self.assertEqual(date_years.iloc[0], 2004)
-
-        with self.subTest("when fetch_data is False"):
-            data_frame = self.data_reader.get_fixture(fetch_data=False)
-
-            self.assertIsInstance(data_frame, pd.DataFrame)
-            self.assertFalse(data_frame.empty)
-            self.assertEqual(data_frame["date"].dtype, "datetime64[ns]")
-
-        with self.subTest("when fetch_data is False and year_range is specified"):
-            data_frame = self.data_reader.get_fixture(
-                fetch_data=False, year_range=(2018, 2019)
-            )
-
-            self.assertIsInstance(data_frame, pd.DataFrame)
-            seasons = data_frame["season"].drop_duplicates()
-            self.assertEqual(len(seasons), 1)
-            self.assertEqual(seasons.iloc[0], 2018)
-
     def test_get_betting_odds(self):
         with self.subTest("when fetch_data is True"):
             data_frame = self.data_reader.get_betting_odds(

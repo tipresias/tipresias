0f41dae2e2dd408a34555d0aa8645c6d87ffca9b
Craig Franklin <craigjfranklin@gmail.com>
2019-06-01 11:11:05 +1000

    Update data import tests

--- backend/machine_learning/tests/integration/data_import/test_afl_data_importer.py
+++ backend/machine_learning/tests/integration/data_import/test_afl_data_importer.py
@@ -1,14 +1,12 @@
-from unittest import skip
 from django.test import TestCase
 import pandas as pd
 
 from machine_learning.data_import import AflDataImporter
 
 
-@skip("I'll need to set up afl_data in CI to get these importer tests working")
 class TestAflDataImporter(TestCase):
     def setUp(self):
-        self.data_importer = AflDataImporter()
+        self.data_importer = AflDataImporter(verbose=0)
 
     def test_get_rosters(self):
         data_frame = self.data_importer.fetch_rosters(1)
--- backend/machine_learning/tests/integration/data_import/test_footywire_data_importer.py
+++ backend/machine_learning/tests/integration/data_import/test_footywire_data_importer.py
@@ -1,4 +1,3 @@
-from unittest import skip
 import os
 from django.test import TestCase
 import pandas as pd
@@ -8,13 +7,9 @@ from machine_learning.data_import import FootywireDataImporter
 DATA_DIR = os.path.abspath(os.path.join(os.path.dirname(__file__), "../../fixtures"))
 
 
-@skip(
-    "As of 5-5-2019, these tests pass on local but hang in CI, so skipping until I "
-    "feel like figuring out what the hell is going on"
-)
 class TestFootywireDataImporter(TestCase):
     def setUp(self):
-        self.data_reader = FootywireDataImporter(csv_dir=DATA_DIR)
+        self.data_reader = FootywireDataImporter(csv_dir=DATA_DIR, verbose=0)
 
     def test_get_betting_odds(self):
         with self.subTest("when fetch_data is True"):
@@ -25,12 +20,12 @@ class TestFootywireDataImporter(TestCase):
             self.assertIsInstance(data_frame, pd.DataFrame)
 
             seasons = data_frame["season"].drop_duplicates()
-            self.assertEqual(len(seasons), 1)
+            self.assertEqual(len(seasons), 2)
             self.assertEqual(seasons.iloc[0], 2014)
 
-            self.assertEqual(data_frame["date"].dtype, "datetime64[ns]")
+            self.assertEqual(data_frame["date"].dtype, "datetime64[ns, UTC+11:00]")
             date_years = data_frame["date"].dt.year.drop_duplicates()
-            self.assertEqual(len(date_years), 1)
+            self.assertEqual(len(date_years), 2)
             self.assertEqual(date_years.iloc[0], 2014)
 
         with self.subTest("when fetch_data is False"):
@@ -38,7 +33,7 @@ class TestFootywireDataImporter(TestCase):
 
             self.assertIsInstance(data_frame, pd.DataFrame)
             self.assertFalse(data_frame.empty)
-            self.assertEqual(data_frame["date"].dtype, "datetime64[ns]")
+            self.assertEqual(data_frame["date"].dtype, "datetime64[ns, UTC+11:00]")
 
         with self.subTest("when fetch_data is False and year_range is specified"):
             data_frame = self.data_reader.get_betting_odds(

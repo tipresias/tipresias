a34379c7dce38ee53fa149cc4a5b1dec8561f8d7
Craig Franklin <craigjfranklin@gmail.com>
2019-06-01 11:31:54 +1000

    Update ml estimator test

--- backend/machine_learning/tests/integration/test_ml_estimators.py
+++ backend/machine_learning/tests/integration/test_ml_estimators.py
@@ -1,58 +1,20 @@
-from unittest import skip
 from django.test import TestCase
+from sklearn.externals import joblib
 
-from machine_learning.ml_data import JoinedMLData
-from machine_learning.ml_estimators import BenchmarkEstimator, BaggingEstimator
-from machine_learning.tests.helpers import regression_accuracy
+from machine_learning.ml_estimators.base_ml_estimator import BaseMLEstimator
+
+PICKLE_FILEPATHS = [
+    "machine_learning/ml_estimators/bagging_estimator/tipresias.pkl",
+    "machine_learning/ml_estimators/benchmark_estimator/benchmark_estimator.pkl",
+]
 
 
-@skip(
-    "These tests are mostly a sanity check for data transformations, but they take way too long, "
-    "and the problems caught here would probably be caught while working on data or model changes."
-)
 class TestMLEstimators(TestCase):
+    """Basic spot check for being able to load saved ML estimators"""
+
     def setUp(self):
-        self.estimators = [
-            (
-                BenchmarkEstimator(),
-                JoinedMLData(train_years=(2010, 2015), test_years=(2016, 2016)),
-            ),
-            (
-                BaggingEstimator(),
-                JoinedMLData(train_years=(2010, 2015), test_years=(2016, 2016)),
-            ),
-        ]
+        self.estimators = [joblib.load(filepath) for filepath in PICKLE_FILEPATHS]
 
-    def test_model_and_data(self):
-        for estimator, data in self.estimators:
-            test_label = (
-                estimator.__class__.__name__ + " and " + data.__class__.__name__
-            )
-
-            with self.subTest(test_label):
-                X_train, y_train = data.train_data()
-
-                if (
-                    "pipeline__correlationselector__labels"
-                    in estimator.get_params().keys()
-                ):
-                    estimator.set_params(pipeline__correlationselector__labels=y_train)
-
-                estimator.fit(X_train, y_train)
-                X_test, y_test = data.test_data()
-                y_pred = estimator.predict(X_test)
-
-                unique_predictions = set(y_pred)
-
-                # If all the predictions are the same, something went horribly wrong
-                self.assertNotEqual(len(unique_predictions), 1)
-
-                accuracy = regression_accuracy(y_test, y_pred)
-
-                # A spot check for leaking data from the match-to-predict
-                # (i.e. accidentally including data from 'future' matches in a data row
-                # that the model is predicting on).
-                # The threshold might change if the models get better, but for now,
-                # whenever I see accuracy > 80%, it's always been due to a mistake
-                # of this nature.
-                self.assertLess(accuracy, 0.8)
+    def test_estimator_validity(self):
+        for estimator in self.estimators:
+            self.assertIsInstance(estimator, BaseMLEstimator)

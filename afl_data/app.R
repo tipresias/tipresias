3560a2f409347a772b880aa98b283809b2851398
Craig Franklin <craigjfranklin@gmail.com>
2019-06-03 18:14:30 +1000

    Make afl_data deployable on Google Cloud Run

--- afl_data/app.R
+++ afl_data/app.R
@@ -1,4 +1,5 @@
 library("magrittr")
 
+port <- as.numeric(Sys.getenv("PORT", unset = "8080"))
 pr <- plumber::plumb("R/plumber.R")
-pr$run(host = "0.0.0.0", port = 8001)
+pr$run(host = "0.0.0.0", port = port)

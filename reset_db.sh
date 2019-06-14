8a0452c6ff5e20014a552de89ebd85720cd4c15f
Craig Franklin <cfranklin@marketplacer.com>
2019-06-14 11:44:02 +1000

    Add db reset script

---
+++ reset_db.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+DB_FILE=dump_`date +%d-%m-%Y"_"%H_%M_%S`.sql
+
+ssh ${PROD_USER}@${IP_ADDRESS} "docker exec -t -u postgres tipresias_db_1 pg_dumpall -c > ~/backups/${DB_FILE}"
+scp ${PROD_USER}@${IP_ADDRESS}:~/backups/${DB_FILE} $PWD
+ssh ${PROD_USER}@${IP_ADDRESS} "rm ~/backups/${DB_FILE}"
+
+docker-compose rm -s -v db
+docker-compose up -d db
+# Not ideal, but wait-for-it was listening for the port to be ready, but that
+# wasn't enough time for the DB to be ready to accept commands,
+# so we're sleeping instead
+sleep 3
+cat ${DB_FILE} | docker exec -i tipresias_db_1 psql -Upostgres

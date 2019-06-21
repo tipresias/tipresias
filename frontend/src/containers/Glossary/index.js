bc739413a68ae341e39257bbf06cde2cadf30f56
melissa gattoni <meligatt@gmail.com>
2019-06-21 22:27:13 +1000

    add react router with nested routes

--- frontend/src/containers/Glossary/index.js
+++ frontend/src/containers/Glossary/index.js
@@ -1,19 +1,52 @@
 import React from 'react';
-import GlossaryStyled from './style';
+import { Route, Link } from 'react-router-dom';
+// import GlossaryStyled from './style';
 
-const Glossary = () => (
-  <GlossaryStyled>
-    <dl>
-      <dt>term 1</dt>
-      <dd>definition 1</dd>
-      <dt>term 2</dt>
-      <dd>definition 2</dd>
-      <dt>term 3</dt>
-      <dd>definition 3</dd>
-      <dt>term 4</dt>
-      <dd>definition 4</dd>
-    </dl>
-  </GlossaryStyled>
+const Topic = ({ match }) => (
+  <h3>
+    Requested Param:
+    {match.params.id}
+  </h3>
 );
+
+const Glossary = ({ match }) => {
+  console.log('match >>>', match);
+
+  return (
+    <div>
+      <h2>Topics</h2>
+
+      <ul>
+        <li>
+          <Link to={`${match.url}/1`}>Tip point</Link>
+        </li>
+        <li>
+          <Link to={`${match.url}/2`}>predicted margin</Link>
+        </li>
+      </ul>
+
+      <Route path={`${match.path}/:id`} component={Topic} />
+      <Route
+        exact
+        path={match.path}
+        render={() => <h3>Please select a Term.</h3>}
+      />
+    </div>
+  );
+};
+// const Glossary = () => (
+//   <GlossaryStyled>
+//     <dl>
+//       <dt>term 1</dt>
+//       <dd>definition 1</dd>
+//       <dt>term 2</dt>
+//       <dd>definition 2</dd>
+//       <dt>term 3</dt>
+//       <dd>definition 3</dd>
+//       <dt>term 4</dt>
+//       <dd>definition 4</dd>
+//     </dl>
+//   </GlossaryStyled>
+// );
 
 export default Glossary;

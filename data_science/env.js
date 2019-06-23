018b39152553c0e38656030caa2105fee61918d8
Craig Franklin <craigjfranklin@gmail.com>
2019-06-23 19:57:52 +1000

    Update serverless config

---
+++ data_science/env.js
@@ -0,0 +1,13 @@
+'use strict'
+
+const path = require('path')
+
+module.exports.env = () => (
+  {
+    project: process.env.PROJECT_ID,
+    credentials: path.resolve(__dirname, process.env.GOOGLE_APPLICATION_CREDENTIALS),
+    gcpf_token: process.env.GCPF_TOKEN,
+    ml_service: process.env.ML_SERVICE_NAME,
+    afl_data_service: process.env.AFL_DATA_SERVICE,
+  }
+)

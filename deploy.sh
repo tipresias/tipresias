d10abf9a5f3b3dcbbe5a07d762aa8e600b9e24c2
Craig Franklin <craigjfranklin@gmail.com>
2019-06-23 20:37:53 +1000

    Change to browser_test service & pull image in CI

--- deploy.sh
+++ deploy.sh
@@ -2,6 +2,7 @@ echo "$DOCKER_PASSWORD" | docker login -u cfranklin11 --password-stdin
 docker push cfranklin11/tipresias_server:master
 docker push cfranklin11/tipresias_client:master
 docker push cfranklin11/tipresias_afl_data:latest
+docker push cfranklin11/tipresias_browser_test:latest
 
 docker pull cfranklin11/tipresias_app:latest
 docker build --cache-from cfranklin11/tipresias_app:latest -t cfranklin11/tipresias_app:latest .

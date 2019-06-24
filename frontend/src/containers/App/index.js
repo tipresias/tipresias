51fe44a4af1acde85bec06008d905fbb9a0c9dae
melissa gattoni <meligatt@gmail.com>
2019-06-24 13:37:15 +1000

    Update glossary client routes

--- frontend/src/containers/App/index.js
+++ frontend/src/containers/App/index.js
@@ -1,42 +1,13 @@
 // @flow
 import React from 'react';
-import { Route, Link, BrowserRouter as Router } from 'react-router-dom';
+import { Route, BrowserRouter as Router } from 'react-router-dom';
 import PageHeader from '../../components/PageHeader';
 import PageFooter from '../../components/PageFooter';
 import Dashboard from '../Dashboard';
-// import Glossary from '../Glossary';
+import Glossary from '../Glossary';
 import About from '../About';
 import { AppContainerStyled, MainStyled } from './style';
 
-const Topic = ({ match }) => (
-  <div>
-    <h3>
-      Requested Param:
-      {match.params.id}
-    </h3>
-  </div>
-);
-
-const Glossary = ({ match }) => {
-  console.log('match >>>', match.url);
-
-  return (
-    <div>
-      <h2>Topics</h2>
-
-      <ul>
-        <li>
-          <Link to="/glossary/1">Tip point</Link>
-        </li>
-        <li>
-          <Link to="/glossary/2">predicted margin</Link>
-        </li>
-      </ul>
-
-      <Route path="/glossary/:id" component={Topic} />
-    </div>
-  );
-};
 const App = () => (
   <AppContainerStyled>
     <Router>
--- frontend/src/containers/Glossary/index.js
+++ frontend/src/containers/Glossary/index.js
@@ -1,52 +1,34 @@
 import React from 'react';
 import { Route, Link } from 'react-router-dom';
-// import GlossaryStyled from './style';
+import GlossaryStyled from './style';
 
 const Topic = ({ match }) => (
-  <h3>
-    Requested Param:
-    {match.params.id}
-  </h3>
+  <div>
+    <h3>
+      Topic:
+      {match.params.id}
+    </h3>
+  </div>
 );
 
-const Glossary = ({ match }) => {
-  console.log('match >>>', match);
+const Glossary = ({ match }) => (
+  <GlossaryStyled>
+    <h2>Topics</h2>
+    <ul>
+      <li>
+        <Link to={`${match.url}/1`}>Tip point</Link>
+      </li>
+      <li>
+        <Link to={`${match.url}/2`}>another term</Link>
+      </li>
+      <li>
+        <Link to={`${match.url}/3`}>another term</Link>
+      </li>
+    </ul>
 
-  return (
-    <div>
-      <h2>Topics</h2>
+    <Route path="/glossary/:id" component={Topic} />
+  </GlossaryStyled>
+);
 
-      <ul>
-        <li>
-          <Link to={`${match.url}/1`}>Tip point</Link>
-        </li>
-        <li>
-          <Link to={`${match.url}/2`}>predicted margin</Link>
-        </li>
-      </ul>
-
-      <Route path={`${match.path}/:id`} component={Topic} />
-      <Route
-        exact
-        path={match.path}
-        render={() => <h3>Please select a Term.</h3>}
-      />
-    </div>
-  );
-};
-// const Glossary = () => (
-//   <GlossaryStyled>
-//     <dl>
-//       <dt>term 1</dt>
-//       <dd>definition 1</dd>
-//       <dt>term 2</dt>
-//       <dd>definition 2</dd>
-//       <dt>term 3</dt>
-//       <dd>definition 3</dd>
-//       <dt>term 4</dt>
-//       <dd>definition 4</dd>
-//     </dl>
-//   </GlossaryStyled>
-// );
 
 export default Glossary;

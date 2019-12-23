031df7035bab3579019fd4d5d06a9003e82e348b
Craig Franklin <craigjfranklin@gmail.com>
2019-12-23 19:15:56 +1100

    Use public dockerhub image for GC app
    I couldn't get authentication/permissions working in compute
    engine, so trying to pull private gcr images always returned
    permission denied. So, might as well just use a public dockerhub
    image instead.

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -2,9 +2,11 @@
 
 set -euo pipefail
 
-DOCKER_IMAGE="gcr.io/${PROJECT_ID}/tipresias:latest"
+DOCKER_IMAGE=cfranklin11/tipresias_app:latest
 
-gcloud auth configure-docker
-docker pull $DOCKER_IMAGE
-docker build --cache-from $DOCKER_IMAGE -t $DOCKER_IMAGE .
-docker push $DOCKER_IMAGE
+docker pull ${DOCKER_IMAGE}
+docker build --cache-from ${DOCKER_IMAGE} -t ${DOCKER_IMAGE} .
+docker push ${DOCKER_IMAGE}
+
+gcloud auth activate-service-account ${GC_SERVICE_ACCOUNT} --key-file=${GOOGLE_APPLICATION_CREDENTIALS}
+gcloud beta compute instances update-container ${PROJECT_ID}-app --container-image ${DOCKER_IMAGE} --zone australia-southeast1-b --project ${PROJECT_ID}

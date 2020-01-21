98e7dce9d0897bd5bcaf94ca61fe7f9ba32ad726
Craig Franklin <craigjfranklin@gmail.com>
2020-01-21 16:31:58 +1100

    Increase instance size
    It seems the GC is running a bunch of other stuff that use up
    memory, so we need a slightly larger server instance for things
    to work properly.

--- scripts/create_instance.sh
+++ scripts/create_instance.sh
@@ -1,7 +1,7 @@
 gcloud beta compute instances create-with-container tipresias-app \
   --container-env DATA_SCIENCE_SERVICE=${DATA_SCIENCE_SERVICE},DATABASE_URL=${DATABASE_URL},DJANGO_SETTINGS_MODULE=project.settings.production,EMAIL_RECIPIENT=${EMAIL_RECIPIENT},FOOTY_TIPS_USERNAME=${FOOTY_TIPS_USERNAME},FOOTY_TIPS_PASSWORD=${FOOTY_TIPS_PASSWORD},GCPF_TOKEN=${GCPF_TOKEN},NODE_ENV=production,PRODUCTION_HOST=${PRODUCTION_HOST},PROJECT_ID=${PROJECT_ID},PYTHON_ENV=production,SENDGRID_API_KEY=${SENDGRID_API_KEY},SECRET_KEY=${SECRET_KEY}\
   --container-image ${DOCKER_IMAGE} \
-  --machine-type f1-micro \
+  --machine-type g1-small \
   --network-tier STANDARD \
   --zone australia-southeast1-b \
   --tags http-server,https-server \

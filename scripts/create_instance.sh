dfb743c53ab3c1616f7b04db96340f79f4a49be4
Craig Franklin <craigjfranklin@gmail.com>
2020-02-09 20:04:01 +1100

    Clean up script files

--- scripts/create_instance.sh
+++ scripts/create_instance.sh
@@ -2,7 +2,7 @@
 
 set -euo pipefail
 
-gcloud beta compute instances create-with-container tipresias-app \
+gcloud beta compute instances create-with-container ${PROJECT_ID}-app \
   --container-env DATA_SCIENCE_SERVICE=${DATA_SCIENCE_SERVICE},DATABASE_URL=${DATABASE_URL},DJANGO_SETTINGS_MODULE=project.settings.production,EMAIL_RECIPIENT=${EMAIL_RECIPIENT},FOOTY_TIPS_USERNAME=${FOOTY_TIPS_USERNAME},FOOTY_TIPS_PASSWORD=${FOOTY_TIPS_PASSWORD},GCPF_TOKEN=${GCPF_TOKEN},NODE_ENV=production,PRODUCTION_HOST=${PRODUCTION_HOST},PROJECT_ID=${PROJECT_ID},PYTHON_ENV=production,SENDGRID_API_KEY=${SENDGRID_API_KEY},SECRET_KEY=${SECRET_KEY}\
   --container-image ${DOCKER_IMAGE} \
   --machine-type g1-small \
--- scripts/dump_local_db.sh
+++ scripts/dump_local_db.sh
@@ -5,5 +5,6 @@ set -euo pipefail
 DB_FILE=local_dump_`date +%Y-%m-%d"_"%H_%M_%S`.sql
 
 # Including a bunch of extra options for compatibility with Google Cloud SQL
-docker exec -t -u postgres tipresias_db_1 pg_dump -U postgres --format=plain --no-owner --no-acl tipresias \
+docker exec -t -u postgres tipresias_db_1 \
+  pg_dump -U postgres --format=plain --no-owner --no-acl tipresias \
   | sed -E 's/(DROP|CREATE|COMMENT ON) EXTENSION/-- \1 EXTENSION/g' > "db/backups/${DB_FILE}"

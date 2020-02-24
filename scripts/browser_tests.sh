4f98e59aabd134c14328e3a0abdaac7e811f1480
Craig Franklin <craigjfranklin@gmail.com>
2020-02-25 08:56:11 +1100

    Add upload of test screenshots to Google Cloud
    I'm having trouble debugging cypress test failures in CI,
    and Travis doesn't seem to have a way to just display images,
    so I'm uploading them to a bucket for review later.

--- scripts/browser_tests.sh
+++ scripts/browser_tests.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-set -euo pipefail
-
 docker-compose -f docker-compose.ci.yml stop
 docker-compose -f docker-compose.ci.yml up -d
 
@@ -11,4 +9,24 @@ docker-compose -f docker-compose.ci.yml up -d
 
 docker-compose -f docker-compose.ci.yml run --rm browser_test npx cypress run
 
+EXIT_CODE=$?
+
 docker-compose -f docker-compose.ci.yml stop
+
+if [ ${EXIT_CODE} -eq 0 ]
+then
+  exit ${EXIT_CODE}
+fi
+
+LOCAL_DIR=${PWD}/browser_test/cypress/screenshots
+BUCKET_DIR=`date +%Y-%m-%d"_"%H_%M_%S`
+
+if [ -d "${LOCAL_DIR}" ]
+then
+  gcloud auth activate-service-account ${GC_SERVICE_ACCOUNT} \
+    --key-file=${GOOGLE_APPLICATION_CREDENTIALS}
+
+  gsutil cp -r ${LOCAL_DIR} gs://${PROJECT_ID}_travis_artifacts/${BUCKET_DIR}
+fi
+
+exit ${EXIT_CODE}

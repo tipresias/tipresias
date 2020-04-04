93beabad565589c33c4008baea5f275ce2a5795b
melissa gattoni <meligatt@gmail.com>
2020-04-05 08:41:10 +1000

    update cypres theme case with correct css selector

--- browser_test/cypress/integration/spec.js
+++ browser_test/cypress/integration/spec.js
@@ -15,7 +15,6 @@ describe("HomePage", function(){
   it("toggles to dark theme", () => {
     cy.get("button[class*=ToggleThemeButton]").click();
     cy.wait(1000);
-    // cy.get("button[class*=ToggleThemeButton]").should('have.css', 'background-color', 'rgb(255, 255, 255)');
-    cy.get("button[class*=ToggleThemeButton]").should('have.css', 'background-color', '#15202b');
+    cy.get("[class*=AppContainerStyled]").should('have.css', 'background-color', '#15202b');
   });
 });

4704d65cfcd078f3e5e1ed3b7ab8c739dbc46d13
Craig Franklin <craigjfranklin@gmail.com>
2020-05-09 17:42:24 +1000

    Update deployment scripts

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,22 +1,46 @@
 #!/bin/bash
 
+
 set -euo pipefail
 
-DOCKER_IMAGE=gcr.io/${PROJECT_ID}/${PROJECT_ID}-app
+APP_DIR=/var/www/${PROJECT_ID}
+DOCKER_IMAGE=cfranklin11/${PROJECT_ID}_app:latest
+PORT=80
 
-gcloud auth activate-service-account ${GC_SERVICE_ACCOUNT} \
-  --key-file=${GOOGLE_APPLICATION_CREDENTIALS}
-cat ${GOOGLE_APPLICATION_CREDENTIALS} | docker login -u _json_key --password-stdin gcr.io
+sudo chmod 600 ~/.ssh/deploy_rsa
+sudo chmod 755 ~/.ssh
 
 docker pull ${DOCKER_IMAGE}
 docker build --cache-from ${DOCKER_IMAGE} -t ${DOCKER_IMAGE} .
 docker push ${DOCKER_IMAGE}
 
-gcloud beta compute instances update-container ${PROJECT_ID}-app \
-  --container-image ${DOCKER_IMAGE} \
-  --zone australia-southeast1-b \
-  --project ${PROJECT_ID}
+RUN_APP="
+  cd ${APP_DIR} \
+    && docker pull ${DOCKER_IMAGE} \
+    && docker stop ${PROJECT_ID}_app \
+    && docker container rm ${PROJECT_ID}_app \
+    && docker run \
+      -d \
+      --env-file .env \
+      -p ${PORT}:${PORT} \
+      -e DJANGO_SETTINGS_MODULE=project.settings.production \
+      -e NODE_ENV=production \
+      --name ${PROJECT_ID}_app \
+      ${DOCKER_IMAGE}
+"
 
-./backend/scripts/wait-for-it.sh ${PRODUCTION_HOST}:${PORT:-8000} \
+# We use 'ssh' instead of 'doctl compute ssh' to be able to bypass key checking.
+ssh -i ~/.ssh/deploy_rsa -oStrictHostKeyChecking=no \
+  ${DIGITAL_OCEAN_USER}@${PRODUCTION_HOST} \
+  ${RUN_APP}
+
+if [ $? != 0 ]
+then
+  exit $?
+fi
+
+./backend/scripts/wait-for-it.sh ${PRODUCTION_HOST}:${PORT} \
   -t 60 \
   -- ./scripts/post_deploy.sh
+
+exit $?

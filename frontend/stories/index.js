ab4e8aa5c29c7371c96c71ed4d66cd8585cf0db9
Craig Franklin <craigjfranklin@gmail.com>
2020-05-26 17:17:57 +1000

    Update storybook syntax per change in v5.2
    Trying to debug a storybook error, which turned out to be a
    CRA error, I found that they had changed the standard syntax.
    They still support the old way, but might as well update it while
    I'm here.

--- frontend/stories/index.js
+++ frontend/stories/index.js
@@ -1,7 +1,11 @@
 import React from 'react';
-import { storiesOf } from '@storybook/react';
 
-storiesOf('Button', module)
-  .add('with text', () => (
-    <div>Hello Button again</div>
-  ));
+export default {
+  title: 'Button',
+};
+
+export const WithText = () => <div>Hello Button again</div>;
+
+WithText.story = {
+  name: 'with text',
+};

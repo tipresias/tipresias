17ec3818ed1c795ff9d7ed03a4caf7fa2b4a47c6
Craig Franklin <craigjfranklin@gmail.com>
2020-05-31 15:37:21 +1000

    Implement submitting tips in predictions handler

--- backend/server/tests/integration/test_urls.py
+++ backend/server/tests/integration/test_urls.py
@@ -1,11 +1,14 @@
 # pylint: disable=missing-docstring
 
+from unittest.mock import MagicMock, patch
+
 from django.test import Client, TestCase
 from django.urls import reverse
 import pandas as pd
 
 from server.tests.fixtures import data_factories, factories
 from server.models import Prediction
+from server.tipping import Tipper
 
 
 N_MATCHES = 9
@@ -15,7 +18,12 @@ class TestUrls(TestCase):
     def setUp(self):
         self.client = Client()
 
-    def test_predictions(self):
+    @patch("server.views.Tipper")
+    def test_predictions(self, mock_tipper_class):
+        mock_tipper = Tipper()
+        mock_tipper.submit_tips = MagicMock()
+        mock_tipper_class.return_value = mock_tipper
+
         ml_model = factories.MLModelFactory(name="test_estimator")
         matches = [factories.FullMatchFactory() for _ in range(N_MATCHES)]
         prediction_data = pd.concat(
@@ -36,4 +44,7 @@ class TestUrls(TestCase):
             reverse("predictions"), content_type="application/json", data=predictions
         )
 
+        # It creates predictions
         self.assertEqual(Prediction.objects.count(), N_MATCHES)
+        # It submits tips
+        mock_tipper.submit_tips.assert_called()

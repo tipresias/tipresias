f83158ffa3f2f16309e05dd8a30948a7b338aa1a
Craig Franklin <craigjfranklin@gmail.com>
2020-06-13 18:09:29 +1000

    Add icon for unknown prediction results

--- frontend/src/icons/index.js
+++ frontend/src/icons/index.js
@@ -1,7 +1,9 @@
 import iconCheck from './icon_check.svg';
 import iconCross from './icon_cross.svg';
+import iconQuestion from './icon_question.svg';
 
 export default {
   iconCheck,
   iconCross,
+  iconQuestion,
 };

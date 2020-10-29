ed818b3bf01e2df9cafe344d6ba004be57f7b1bc
Craig Franklin <craigjfranklin@gmail.com>
2020-10-29 15:56:49 +1100

    Add GraphQL query to FaunaDB client
    This required updated other GQL-related packages to make
    everything compatible, as `gql` that's compatible with
    Python > 3.6 requires `graphql-core` >= 3.0, which is only
    compatible with `graphene` et al >=3.0, which is still in
    pre-release, but it seems to work.

--- backend/server/db/faunadb.py
+++ backend/server/db/faunadb.py
@@ -1,10 +1,13 @@
 """Module for all FaunaDB functionality."""
 
-from typing import Literal, Union
+from typing import Literal, Union, Any, Dict, Optional
 import os
+import json
+import logging
 
 import requests
 from django.conf import settings
+from gql import gql, Client, AIOHTTPTransport
 
 ImportMode = Union[Literal["merge"], Literal["overwrite"]]
 
@@ -42,3 +45,41 @@ class FaunadbClient:
                 "X-Schema-Preview": "partial-update-mutation",
             },
         )
+
+    @classmethod
+    def graphql(
+        cls, query: str, variables: Optional[Dict[str, Any]] = None
+    ) -> Dict[str, Any]:
+        """Send a GraphQL query to a FaunaDB endpoint.
+
+        Params:
+        -------
+        query: GraphQL query string
+        """
+        transport = AIOHTTPTransport(
+            url=f"{FAUNADB_DOMAIN}/graphql",
+            headers={
+                "Authorization": f"Bearer {settings.FAUNADB_KEY}",
+                "X-Schema-Preview": "partial-update-mutation",
+            },
+        )
+        graphql_client = Client(transport=transport)
+
+        graphql_query = gql(query)
+        graphql_variables = variables or {}
+
+        try:
+            result = graphql_client.execute(
+                graphql_query, variable_values=graphql_variables
+            )
+        except Exception as err:
+            logging.error(graphql_variables)
+            raise err
+
+        errors = result.get("errors", [])
+
+        if any(errors):
+            logging.error(graphql_variables)
+            raise Exception(json.dumps(errors, indent=2))
+
+        return result
--- backend/server/tests/unit/db/test_faunadb.py
+++ backend/server/tests/unit/db/test_faunadb.py
@@ -15,3 +15,33 @@ class TestFaunaDBClient(TestCase):
         mock_post.assert_called()
         # It sends the schema file
         isinstance(mock_post.mock_calls[0].kwargs["data"], bytes)
+
+    @patch("server.db.faunadb.Client.execute")
+    def test_graphql(self, mock_execute):
+        mock_data = {"createTeam": {"name": "NewTeam"}}
+        mock_execute.return_value = mock_data
+
+        query = """
+            mutation {
+                createTeam(data: { name: "NewTeam" }) { name }
+            }
+        """
+
+        data = FaunadbClient.graphql(query)
+
+        # It returns the GQL query data
+        self.assertEqual(data, mock_data)
+
+        with self.subTest("when there are errors"):
+            mock_errors = {
+                "errors": [
+                    {
+                        "message": "Oops",
+                        "extensions": {"code": "That boy ain't right"},
+                    }
+                ]
+            }
+            mock_execute.return_value = mock_errors
+
+            with self.assertRaisesRegex(Exception, r"Oops"):
+                FaunadbClient.graphql(query)

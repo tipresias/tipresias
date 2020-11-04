c9b377d8746b3857f6929be9caf76d14057e8dcc
Craig Franklin <craigjfranklin@gmail.com>
2020-11-04 20:47:19 +1100

    Move FaunaDB code to tipping service
    Trying to migrate the Django DB connection from Postgres to
    FaunaDB proved more difficult than it was worth. The Django
    DB backends are more-closely tied to relational DBs than I
    realised, such that even a MongoDB alternative converts SQL to
    valid MongoDB queries. Writing a custom Django DB backend for
    FaunaDB ain't worth the squeeze.
    
    Instead of migrating DBs, then migrating functionality from
    backend to tipping, we'll recreate model/DB functionality in
    tipping without the Django baggage.
    
    This commit includes uncommitted changes that I made in backend
    while trying to implement FaunaDB queries in the Team model,
    because I didn't feel like undoing them only to redo them for the
    sake of a pristine git history.

--- tipping/src/tests/unit/db/test_faunadb.py
+++ tipping/src/tests/unit/db/test_faunadb.py
@@ -3,20 +3,24 @@
 from unittest import TestCase
 from unittest.mock import patch
 
-from server.db.faunadb import FaunadbClient
+from tipping.db.faunadb import FaunadbClient
 
 
 class TestFaunaDBClient(TestCase):
-    @patch("server.db.faunadb.requests.post")
+    def setUp(self):
+
+        self.client = FaunadbClient(faunadb_key="notakey")
+
+    @patch("tipping.db.faunadb.requests.post")
     def test_import(self, mock_post):
-        FaunadbClient.import_schema()
+        self.client.import_schema()
 
         # It posts to FaunaDB
         mock_post.assert_called()
         # It sends the schema file
         isinstance(mock_post.mock_calls[0].kwargs["data"], bytes)
 
-    @patch("server.db.faunadb.Client.execute")
+    @patch("tipping.db.faunadb.Client.execute")
     def test_graphql(self, mock_execute):
         mock_data = {"createTeam": {"name": "NewTeam"}}
         mock_execute.return_value = mock_data
@@ -27,7 +31,7 @@ class TestFaunaDBClient(TestCase):
             }
         """
 
-        data = FaunadbClient.graphql(query)
+        data = self.client.graphql(query)
 
         # It returns the GQL query data
         self.assertEqual(data, mock_data)
@@ -44,4 +48,4 @@ class TestFaunaDBClient(TestCase):
             mock_execute.return_value = mock_errors
 
             with self.assertRaisesRegex(Exception, r"Oops"):
-                FaunadbClient.graphql(query)
+                self.client.graphql(query)

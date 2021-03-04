a7510e1406034f71b9d27d5a769d014e07c60c68
Craig Franklin <craigjfranklin@gmail.com>
2021-03-04 21:43:10 +1100

    WIP

--- scripts/integration_tests.sh
+++ scripts/integration_tests.sh
@@ -5,6 +5,10 @@ set -euo pipefail
 DOCKER_COMPOSE_FILE="${1:-docker-compose.yml}"
 
 docker-compose -f ${DOCKER_COMPOSE_FILE} stop
+
+docker-compose pull
+docker-compose up --no-start
+
 docker-compose -f ${DOCKER_COMPOSE_FILE} up -d
 ./scripts/wait-for-it.sh localhost:3000 -t 30 -- echo "Server ready"
 

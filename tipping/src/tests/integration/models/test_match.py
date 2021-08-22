4cc170bf527442b42770fbff59ba9861c664e5be
Craig Franklin <craigjfranklin@gmail.com>
2021-08-22 14:00:24 +1000

    Use factories in model integration tests

--- tipping/src/tests/integration/models/test_match.py
+++ tipping/src/tests/integration/models/test_match.py
@@ -8,15 +8,15 @@ from sqlalchemy import select, func, sql
 from freezegun import freeze_time
 import pytest
 
-from tests.fixtures import data_factories
-from tipping import models
+from tests.fixtures import data_factories, model_factories
+from tipping.models import Match
 
 
 Fake = Faker()
 
 
 def test_match_creation(fauna_session):
-    match = models.Match(
+    match = Match(
         start_date_time=Fake.date_time(tzinfo=timezone.utc),
         round_number=np.random.randint(1, 100),
         venue=Fake.company(),
@@ -37,9 +37,9 @@ def test_from_future_fixtures(fauna_session):
     )
 
     with freeze_time(patched_date):
-        assert fauna_session.execute(select(func.count(models.Match.id))).scalar() == 0
+        assert fauna_session.execute(select(func.count(Match.id))).scalar() == 0
 
-        matches = models.Match.from_future_fixtures(
+        matches = Match.from_future_fixtures(
             fauna_session, upcoming_fixture_matches, upcoming_round_number
         )
         created_match_count = len(matches)
@@ -54,14 +54,12 @@ def test_from_future_fixtures(fauna_session):
         fauna_session.commit()
 
         # With existing matches in DB
-        matches = models.Match.from_future_fixtures(
+        matches = Match.from_future_fixtures(
             fauna_session, upcoming_fixture_matches, upcoming_round_number
         )
 
         assert len(matches) == 0
-        total_match_count = fauna_session.execute(
-            select(func.count(models.Match.id))
-        ).scalar()
+        total_match_count = fauna_session.execute(select(func.count(Match.id))).scalar()
         assert total_match_count == created_match_count
 
 
@@ -92,7 +90,7 @@ def test_from_future_fixtures_with_skipped_round(fauna_session):
     upcoming_round_number = int(next_match["round_number"]) + 1
 
     fauna_session.add(
-        models.Match(
+        Match(
             start_date_time=first_match["date"].to_pydatetime(),
             round_number=first_match["round_number"],
             venue=first_match["venue"],
@@ -105,7 +103,7 @@ def test_from_future_fixtures_with_skipped_round(fauna_session):
             AssertionError,
             match="Expected upcoming round number to be 1 greater than previous round",
         ):
-            models.Match.from_future_fixtures(
+            Match.from_future_fixtures(
                 fauna_session, fixture_matches, upcoming_round_number
             )
             # Logging to catch cause of flaky test
@@ -115,44 +113,31 @@ def test_from_future_fixtures_with_skipped_round(fauna_session):
 
 def test_played_without_results(fauna_session):
     right_now = datetime.now(tz=timezone.utc)
-    two_teams = fauna_session.execute(select(models.Team).limit(2)).scalars().all()
 
-    played_with_results = models.Match(
+    model_factories.FullMatchFactory(
         start_date_time=right_now - timedelta(days=3),
-        venue=Fake.company(),
         round_number=1,
     )
-    for team in two_teams:
-        played_with_results.team_matches.append(
-            models.TeamMatch(team=team, score=np.random.randint(1, 150))
-        )
 
     resultless_matches = []
     for n in range(2):
-        played_without_results = models.Match(
-            start_date_time=right_now - timedelta(days=(n + 2)),
-            venue=Fake.company(),
-            round_number=(n + 2),
-        )
-        for team in two_teams:
-            played_without_results.team_matches.append(
-                models.TeamMatch(team=team, score=None)
+        resultless_matches.append(
+            model_factories.FullMatchFactory(
+                start_date_time=right_now - timedelta(days=(n + 2)),
+                round_number=(n + 2),
+                home_team_match__score=None,
+                away_team_match__score=None,
             )
-        resultless_matches.append(played_without_results)
+        )
 
-    unplayed = models.Match(
-        start_date_time=right_now, venue=Fake.company(), round_number=4
+    model_factories.FullMatchFactory(
+        start_date_time=right_now,
+        round_number=4,
+        home_team_match__score=None,
+        away_team_match__score=None,
     )
-    for team in two_teams:
-        unplayed.team_matches.append(models.TeamMatch(team=team, score=None))
 
-    fauna_session.add(played_with_results)
-    for resultless_match in resultless_matches:
-        fauna_session.add(resultless_match)
-    fauna_session.add(unplayed)
-    fauna_session.commit()
-
-    match_query = models.Match.played_without_results()
+    match_query = Match.played_without_results()
     queried_matches = fauna_session.execute(match_query).scalars().all()
 
     assert len(queried_matches) == 2
@@ -162,34 +147,23 @@ def test_played_without_results(fauna_session):
 
 def test_earliest_without_results(fauna_session):
     right_now = datetime.now(tz=timezone.utc)
-    two_teams = fauna_session.execute(select(models.Team).limit(2)).scalars().all()
 
-    played_without_results = models.Match(
+    model_factories.FullMatchFactory(
         start_date_time=right_now - timedelta(days=1),
-        venue=Fake.company(),
         round_number=2,
+        home_team_match__score=None,
+        away_team_match__score=None,
     )
-    for team in two_teams:
-        played_without_results.team_matches.append(
-            models.TeamMatch(team=team, score=None)
-        )
 
     earliest_date = right_now - timedelta(days=2)
-    earliest_without_results = models.Match(
+    earliest_without_results = model_factories.FullMatchFactory(
         start_date_time=earliest_date,
-        venue=Fake.company(),
         round_number=1,
+        home_team_match__score=None,
+        away_team_match__score=None,
     )
-    for team in two_teams:
-        earliest_without_results.team_matches.append(
-            models.TeamMatch(team=team, score=None)
-        )
 
-    fauna_session.add(played_without_results)
-    fauna_session.add(earliest_without_results)
-    fauna_session.commit()
-
-    query = models.Match.earliest_without_results()
+    query = Match.earliest_without_results()
     queried_matches = fauna_session.execute(query).scalars().all()
     assert len(queried_matches) == 1
     assert queried_matches[0] == earliest_without_results
@@ -201,48 +175,18 @@ def test_update_results(fauna_session):
     matches = []
 
     for _idx, match_result in match_results_to_update.iterrows():
-        home_team, away_team = [
-            fauna_session.execute(
-                sql.select(models.Team).where(models.Team.name == match_result[team])
-            )
-            .scalars()
-            .first()
-            for team in ["home_team", "away_team"]
-        ]
-
-        match = models.Match(
+        match = model_factories.FullMatchFactory(
             start_date_time=match_result["date"],
-            venue=Fake.company(),
             round_number=match_result["round_number"],
-            team_matches=[
-                models.TeamMatch(
-                    team=home_team,
-                    at_home=True,
-                ),
-                models.TeamMatch(
-                    team=away_team,
-                    at_home=False,
-                ),
-            ],
-            predictions=[
-                models.Prediction(
-                    ml_model=models.MLModel(
-                        name=Fake.company(), prediction_type="margin"
-                    ),
-                    predicted_winner=home_team,
-                    predicted_margin=np.random.randint(0, 100),
-                )
-            ],
+            home_team_match__team__name=match_result["home_team"],
+            away_team_match__team__name=match_result["away_team"],
         )
+
         matches.append(match)
-        fauna_session.add(match)
 
-    fauna_session.commit()
+    Match.update_results(matches, match_results)
 
-    models.Match.update_results(matches, match_results)
-    fauna_session.commit()
-
-    queried_matches = fauna_session.execute(sql.select(models.Match)).scalars().all()
+    queried_matches = fauna_session.execute(sql.select(Match)).scalars().all()
     for match in queried_matches:
         assert match.margin is not None
         assert match.winner_id is not None

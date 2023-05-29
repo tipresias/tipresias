ffdd24f6abf43369a1e11ca7efb61b3d86d3d132
Craig Franklin <craigjfranklin@gmail.com>
2023-05-29 21:45:42 +1000

    Install Chackra UI & dependencies & add boilerplate
    Chakra UI requires a whole bunch of boilerplate to work with Remix,
    so I copied all of that over from the documentation. One of the hooks
    that Chakra UI uses has an empty dependency array, so I disabled
    that rule in ESLint, because it's one I skip more often than adhere to.

--- app/entry.server.tsx
+++ app/entry.server.tsx
@@ -1,6 +1,12 @@
-import type { EntryContext } from "@remix-run/node";
+import { CacheProvider } from "@emotion/react";
+import createEmotionServer from "@emotion/server/create-instance";
 import { RemixServer } from "@remix-run/react";
 import { renderToString } from "react-dom/server";
+
+import { ServerStyleContext } from "./context";
+import createEmotionCache from "./createEmotionCache";
+
+import type { EntryContext } from "@remix-run/node";
 
 export default function handleRequest(
   request: Request,
@@ -8,8 +14,25 @@ export default function handleRequest(
   responseHeaders: Headers,
   remixContext: EntryContext
 ) {
+  const cache = createEmotionCache();
+  const { extractCriticalToChunks } = createEmotionServer(cache);
+
+  const html = renderToString(
+    <ServerStyleContext.Provider value={null}>
+      <CacheProvider value={cache}>
+        <RemixServer context={remixContext} url={request.url} />
+      </CacheProvider>
+    </ServerStyleContext.Provider>
+  );
+
+  const chunks = extractCriticalToChunks(html);
+
   const markup = renderToString(
-    <RemixServer context={remixContext} url={request.url} />
+    <ServerStyleContext.Provider value={chunks.styles}>
+      <CacheProvider value={cache}>
+        <RemixServer context={remixContext} url={request.url} />
+      </CacheProvider>
+    </ServerStyleContext.Provider>
   );
 
   responseHeaders.set("Content-Type", "text/html");

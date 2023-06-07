e43b854f85f3a58bce961c41d7c17d6106fff21b
Craig Franklin <craigjfranklin@gmail.com>
2023-06-07 21:49:22 +1000

    Add MetricsTable component

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,6 +1,7 @@
-import { Container, Flex, Heading, Text } from "@chakra-ui/react";
+import { Box, Container, Flex, Heading, Text } from "@chakra-ui/react";
 import { json } from "@remix-run/node";
 import { useLoaderData } from "@remix-run/react";
+import MetricsTable from "~/components/MetricsTable";
 
 import PredictionsTable from "~/components/PredictionsTable";
 
@@ -13,25 +14,37 @@ export const loader = async () => {
       margin: i ** 2,
       wasCorrect: i === 0 ? null : Boolean(i % 2),
     })),
+    metrics: [
+      { name: "Total Tips", value: "42" },
+      { name: "MAE", value: "21.2" },
+      { name: "Accuracy", value: "84.8%" },
+    ],
+    currentSeason: 2023,
   });
 };
 
 export default function Index() {
-  const { currentRound, predictions } = useLoaderData<typeof loader>();
+  const { currentRound, predictions, metrics, currentSeason } =
+    useLoaderData<typeof loader>();
 
   return (
     <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.4" }}>
-      <Container centerContent={true} style={{ padding: "2rem" }}>
+      <Container centerContent={true} padding="2rem">
         <Heading as="h1">Tipresias</Heading>
         <Text>A footy-tipping machine-learning model</Text>
       </Container>
-      <Flex alignItems="center" flexDirection="column">
-        {predictions && (
-          <PredictionsTable
-            currentRound={currentRound}
-            predictions={predictions}
-          />
-        )}
+      <Flex alignItems="start" justifyContent="space-around" flexWrap="wrap">
+        <Box padding="1rem">
+          {predictions && (
+            <PredictionsTable
+              currentRound={currentRound}
+              predictions={predictions}
+            />
+          )}
+        </Box>
+        <Box padding="1rem">
+          {metrics && <MetricsTable metrics={metrics} season={currentSeason} />}
+        </Box>
       </Flex>
     </div>
   );
---
+++ tests/components/MetricsTable.test.tsx
@@ -0,0 +1,27 @@
+import { faker } from "@faker-js/faker";
+import { render, screen } from "@testing-library/react";
+
+import MetricsTable from "../../app/components/MetricsTable";
+
+describe("MetricsTable", () => {
+  const metrics = new Array(4).fill(null).map(() => ({
+    name: faker.company.buzzNoun(),
+    value: faker.number.float().toString(),
+  }));
+  const season = faker.number.int();
+
+  it("displays the table title", () => {
+    render(<MetricsTable metrics={metrics} season={season} />);
+
+    screen.getByText(`Model performance for ${season}`);
+  });
+
+  it("displays metrics", () => {
+    render(<MetricsTable metrics={metrics} season={season} />);
+
+    const { name, value } = faker.helpers.arrayElement(metrics);
+
+    screen.getByText(name);
+    screen.getByText(value);
+  });
+});

9e4b0e0169f270278d20cdfb158f0b7cdd8945e6
Craig Franklin <craigjfranklin@gmail.com>
2024-05-24 09:58:25 +1000

    Update remix-run dependencies to latest
    The site is crashing due to a weird error inside remix-run,
    so it's probably just too old, since I haven't updated packages
    in about a year. Unfortunately, this required updating a bunch
    of other packages and rewriting all the Remix boilerplate to match
    the new version of the code generator.
    
    I couldn't quite figure out how to incorporate Chakra UI into
    entry.server.tsx, but it seems to work okay, so I commented out
    the broken code and might come back to it later (but probably not).

--- app/entry.server.tsx
+++ app/entry.server.tsx
@@ -1,44 +1,175 @@
-import { CacheProvider } from "@emotion/react";
-import createEmotionServer from "@emotion/server/create-instance";
+/**
+ * By default, Remix will handle generating the HTTP Response for you.
+ * You are free to delete this file if you'd like to, but if you ever want it revealed again, you can run `npx remix reveal` ✨
+ * For more information, see https://remix.run/file-conventions/entry.server
+ */
+
+import { PassThrough } from "node:stream";
+import { CacheProvider as EmotionCacheProvider } from "@emotion/react";
+import createEmotionCache from "@emotion/cache";
+// TODO: See TODO messages below
+// import createEmotionServer from "@emotion/server/create-instance";
+import type { AppLoadContext, EntryContext } from "@remix-run/node";
+import { createReadableStreamFromReadable } from "@remix-run/node";
 import { RemixServer } from "@remix-run/react";
-import { renderToString } from "react-dom/server";
+import { isbot } from "isbot";
+import { renderToPipeableStream } from "react-dom/server";
 
-import { ServerStyleContext } from "./context";
-import createEmotionCache from "./createEmotionCache";
-
-import type { EntryContext } from "@remix-run/node";
+const ABORT_DELAY = 5_000;
 
 export default function handleRequest(
   request: Request,
   responseStatusCode: number,
   responseHeaders: Headers,
+  remixContext: EntryContext,
+  // This is ignored so we can keep it in the template for visibility.  Feel
+  // free to delete this parameter in your app if you're not using it!
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  loadContext: AppLoadContext
+) {
+  return isbot(request.headers.get("user-agent") || "")
+    ? handleBotRequest(
+        request,
+        responseStatusCode,
+        responseHeaders,
+        remixContext
+      )
+    : handleBrowserRequest(
+        request,
+        responseStatusCode,
+        responseHeaders,
+        remixContext
+      );
+}
+
+function handleBotRequest(
+  request: Request,
+  responseStatusCode: number,
+  responseHeaders: Headers,
   remixContext: EntryContext
 ) {
-  const cache = createEmotionCache();
-  const { extractCriticalToChunks } = createEmotionServer(cache);
+  return new Promise((resolve, reject) => {
+    let shellRendered = false;
+    const emotionCache = createEmotionCache({ key: "css" });
 
-  const html = renderToString(
-    <ServerStyleContext.Provider value={null}>
-      <CacheProvider value={cache}>
-        <RemixServer context={remixContext} url={request.url} />
-      </CacheProvider>
-    </ServerStyleContext.Provider>
-  );
+    const { pipe, abort } = renderToPipeableStream(
+      <EmotionCacheProvider value={emotionCache}>
+        <RemixServer
+          context={remixContext}
+          url={request.url}
+          abortDelay={ABORT_DELAY}
+        />
+      </EmotionCacheProvider>,
+      {
+        onAllReady() {
+          shellRendered = true;
+          const body = new PassThrough();
 
-  const chunks = extractCriticalToChunks(html);
+          // TODO: The example in the remix-run repo for adding Chakra UI is
+          // out of date, and the 'bodyWithStyles' steam is incompatible
+          // with the stream created by 'createReadableStreamFromReadable'.
+          // Figuring out how to get them to play nice would require digging
+          // into package source code and researching the intricacies of
+          // JS streams, and I don't care enough to bother.
+          // Leaving the code from the example here in case I decide to
+          // dedicate the time to figure it out later.
+          // const emotionServer = createEmotionServer(emotionCache);
+          // const bodyWithStyles = emotionServer.renderStylesToNodeStream();
+          // body.pipe(bodyWithStyles)
+          const stream = createReadableStreamFromReadable(body);
 
-  const markup = renderToString(
-    <ServerStyleContext.Provider value={chunks.styles}>
-      <CacheProvider value={cache}>
-        <RemixServer context={remixContext} url={request.url} />
-      </CacheProvider>
-    </ServerStyleContext.Provider>
-  );
+          responseHeaders.set("Content-Type", "text/html");
 
-  responseHeaders.set("Content-Type", "text/html");
+          resolve(
+            new Response(stream, {
+              headers: responseHeaders,
+              status: responseStatusCode,
+            })
+          );
 
-  return new Response("<!DOCTYPE html>" + markup, {
-    headers: responseHeaders,
-    status: responseStatusCode,
+          pipe(body);
+        },
+        onShellError(error: unknown) {
+          reject(error);
+        },
+        onError(error: unknown) {
+          responseStatusCode = 500;
+          // Log streaming rendering errors from inside the shell.  Don't log
+          // errors encountered during initial shell rendering since they'll
+          // reject and get logged in handleDocumentRequest.
+          if (shellRendered) {
+            console.error(error);
+          }
+        },
+      }
+    );
+
+    setTimeout(abort, ABORT_DELAY);
+  });
+}
+
+function handleBrowserRequest(
+  request: Request,
+  responseStatusCode: number,
+  responseHeaders: Headers,
+  remixContext: EntryContext
+) {
+  return new Promise((resolve, reject) => {
+    let shellRendered = false;
+    const emotionCache = createEmotionCache({ key: "css" });
+
+    const { pipe, abort } = renderToPipeableStream(
+      <EmotionCacheProvider value={emotionCache}>
+        <RemixServer
+          context={remixContext}
+          url={request.url}
+          abortDelay={ABORT_DELAY}
+        />
+      </EmotionCacheProvider>,
+      {
+        onShellReady() {
+          shellRendered = true;
+          const body = new PassThrough();
+
+          // TODO: The example in the remix-run repo for adding Chakra UI is
+          // out of date, and the 'bodyWithStyles' steam is incompatible
+          // with the stream created by 'createReadableStreamFromReadable'.
+          // Figuring out how to get them to play nice would require digging
+          // into package source code and researching the intricacies of
+          // JS streams, and I don't care enough to bother.
+          // Leaving the code from the example here in case I decide to
+          // dedicate the time to figure it out later.
+          // const emotionServer = createEmotionServer(emotionCache);
+          // const bodyWithStyles = emotionServer.renderStylesToNodeStream();
+          // body.pipe(bodyWithStyles);
+          const stream = createReadableStreamFromReadable(body);
+
+          responseHeaders.set("Content-Type", "text/html");
+
+          resolve(
+            new Response(stream, {
+              headers: responseHeaders,
+              status: responseStatusCode,
+            })
+          );
+
+          pipe(body);
+        },
+        onShellError(error: unknown) {
+          reject(error);
+        },
+        onError(error: unknown) {
+          responseStatusCode = 500;
+          // Log streaming rendering errors from inside the shell.  Don't log
+          // errors encountered during initial shell rendering since they'll
+          // reject and get logged in handleDocumentRequest.
+          if (shellRendered) {
+            console.error(error);
+          }
+        },
+      }
+    );
+
+    setTimeout(abort, ABORT_DELAY);
   });
 }
--- app/root.tsx
+++ app/root.tsx
@@ -1,10 +1,5 @@
-import { ChakraProvider, cookieStorageManagerSSR } from "@chakra-ui/react";
-import { withEmotionCache } from "@emotion/react";
-import type {
-  LinksFunction,
-  LoaderFunction,
-  MetaFunction,
-} from "@remix-run/node";
+import { ChakraProvider } from "@chakra-ui/react";
+import type { LinksFunction, LoaderFunction } from "@remix-run/node";
 import {
   Links,
   LiveReload,
@@ -12,21 +7,7 @@ import {
   Outlet,
   Scripts,
   ScrollRestoration,
-  useLoaderData,
 } from "@remix-run/react";
-
-import { ServerStyleContext, ClientStyleContext } from "./context";
-import { useContext, useEffect, useMemo } from "react";
-
-interface DocumentProps {
-  children: React.ReactNode;
-}
-
-export const meta: MetaFunction = () => ({
-  charset: "utf-8",
-  title: "Tipresias: A footy-tipping machine-learning model",
-  viewport: "width=device-width,initial-scale=1",
-});
 
 export const links: LinksFunction = () => {
   return [
@@ -46,101 +27,36 @@ export const loader: LoaderFunction = async ({ request }) => {
   return request.headers.get("cookie") ?? "";
 };
 
-const getColorMode = (cookies: string) => {
-  const match = cookies.match(
-    new RegExp(`(^| )${CHAKRA_COOKIE_COLOR_KEY}=([^;]+)`)
+function Document({
+  children,
+  title = "App title",
+}: {
+  children: React.ReactNode;
+  title?: string;
+}) {
+  return (
+    <html lang="en">
+      <head>
+        <Meta />
+        <title>{title}</title>
+        <Links />
+      </head>
+      <body>
+        {children}
+        <ScrollRestoration />
+        <Scripts />
+        <LiveReload />
+      </body>
+    </html>
   );
-  return match == null ? void 0 : match[2];
-};
-
-// here we can set the default color mode. If we set it to null,
-// there's no way for us to know what is the the user's preferred theme
-// so the cient will have to figure out and maybe there'll be a flash the first time the user visits us.
-const DEFAULT_COLOR_MODE: "dark" | "light" | null = "light";
-
-const CHAKRA_COOKIE_COLOR_KEY = "chakra-ui-color-mode";
-
-const Document = withEmotionCache(
-  ({ children }: DocumentProps, emotionCache) => {
-    const serverStyleData = useContext(ServerStyleContext);
-    const clientStyleData = useContext(ClientStyleContext);
-
-    // Only executed on client
-    useEffect(() => {
-      // re-link sheet container
-      emotionCache.sheet.container = document.head;
-      // re-inject tags
-      const tags = emotionCache.sheet.tags;
-      emotionCache.sheet.flush();
-      tags.forEach((tag) => {
-        (emotionCache.sheet as any)._insertTag(tag);
-      });
-      // reset cache to reapply global styles
-      clientStyleData?.reset();
-    }, []);
-
-    let cookies = useLoaderData();
-
-    // the client get the cookies from the document
-    // because when we do a client routing, the loader can have stored an outdated value
-    if (typeof document !== "undefined") {
-      cookies = document.cookie;
-    }
-
-    // get and store the color mode from the cookies.
-    // It'll update the cookies if there isn't any and we have set a default value
-    const colorMode = useMemo(() => {
-      let color = getColorMode(cookies);
-
-      if (!color && DEFAULT_COLOR_MODE) {
-        cookies += ` ${CHAKRA_COOKIE_COLOR_KEY}=${DEFAULT_COLOR_MODE}`;
-        color = DEFAULT_COLOR_MODE;
-      }
-
-      return color;
-    }, [cookies]);
-
-    return (
-      <html
-        lang="en"
-        {...(colorMode && {
-          "data-theme": colorMode,
-          style: { colorScheme: colorMode },
-        })}
-      >
-        <head>
-          <Meta />
-          <Links />
-          {serverStyleData?.map(({ key, ids, css }) => (
-            <style
-              key={key}
-              data-emotion={`${key} ${ids.join(" ")}`}
-              dangerouslySetInnerHTML={{ __html: css }}
-            />
-          ))}
-        </head>
-        <body
-          {...(colorMode && {
-            className: `chakra-ui-${colorMode}`,
-          })}
-        >
-          <ChakraProvider colorModeManager={cookieStorageManagerSSR(cookies)}>
-            {children}
-          </ChakraProvider>
-
-          <ScrollRestoration />
-          <Scripts />
-          <LiveReload />
-        </body>
-      </html>
-    );
-  }
-);
+}
 
 export default function App() {
   return (
     <Document>
-      <Outlet />
+      <ChakraProvider>
+        <Outlet />
+      </ChakraProvider>
     </Document>
   );
 }

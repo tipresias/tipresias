7c5e20a4cca23575408580cd104a235edbcc182b
Craig Franklin <craigjfranklin@gmail.com>
2024-06-08 12:54:55 +1000

    Add info tooltip explaining prediction numbers
    Predicting a win and a loss for the same team can be confusing,
    so I just added a little info tooltip to explain why that can
    happen.

--- app/components/PredictionsTable.tsx
+++ app/components/PredictionsTable.tsx
@@ -6,11 +6,13 @@ import {
   Td,
   Th,
   Thead,
+  Tooltip,
   Tr,
 } from "@chakra-ui/react";
 
 import { RoundPrediction } from "../.server/predictionService";
 import { presentNumber, presentPercentage } from "../helpers/number";
+import { InfoOutlineIcon } from "@chakra-ui/icons";
 
 interface PredictionsTableProps {
   currentRound: number;
@@ -28,6 +30,20 @@ export const displayCorrectness = (wasCorrect: boolean | null) => {
       return "dunno";
   }
 };
+
+const PREDICTION_INFO = `Margin and win probability are generated
+  by different models that sometimes disagree. In those cases,
+  the secondary prediction will be for a loss for the given team,
+  resulting in negative margins or win probabilities of less than 0.5.`;
+const PredictionInfoTooltip = () => (
+  <Tooltip
+    label={PREDICTION_INFO}
+    aria-label="Prediction info tooltip"
+    placement="top-start"
+  >
+    <InfoOutlineIcon />
+  </Tooltip>
+);
 
 const PredictionRow = ({
   predictedWinnerName,
@@ -50,7 +66,8 @@ const PredictionsTable = ({
 }: PredictionsTableProps) => (
   <TableContainer textAlign="center">
     <Heading as="h2" size="l" margin="0.5rem" whiteSpace="nowrap">
-      Predictions for round {currentRound}, {currentSeason}
+      Predictions for round {currentRound}, {currentSeason}{" "}
+      <PredictionInfoTooltip />
     </Heading>
     <Table variant="striped" maxWidth="fit-content">
       <Thead>
